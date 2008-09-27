58d1a65408ee32d590071c2ced7260b5592b5d3c
Matthew Wild <mwild1@gmail.com>
2008-09-27 19:17:40 +0100

    Fixing jid.split() for all JIDs

--- util/jid.lua
+++ util/jid.lua
@@ -4,5 +4,8 @@ local match = string.match;
 module "jid"
 
 function split(jid)
-	return match(jid, "^([^@]+)@([^/]+)/?(.*)$");
+	local node = match(jid, "^([^@]+)@");
+	local server = (node and match(jid, ".-@([^@/]+)")) or match(jid, "^([^@/]+)");
+	local resource = match(jid, "/(.+)$");
+	return node, server, resource;
 end
