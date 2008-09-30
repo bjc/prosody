67e37801b9a754743a42fe95ae6b54004090c39d
Matthew Wild <mwild1@gmail.com>
2008-09-30 19:49:36 +0100

    jid.split(): Return nil when passed nil

--- util/jid.lua
+++ util/jid.lua
@@ -4,6 +4,7 @@ local match = string.match;
 module "jid"
 
 function split(jid)
+	if not jid then return nil; end
 	local node = match(jid, "^([^@]+)@");
 	local server = (node and match(jid, ".-@([^@/]+)")) or match(jid, "^([^@/]+)");
 	local resource = match(jid, "/(.+)$");
