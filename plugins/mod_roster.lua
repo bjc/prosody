4318fc935bdb14020dd964629873f1059ce48b67
Waqas Hussain <waqas20@gmail.com>
2008-10-08 20:38:05 +0500

    Removed: Unnecessary check in mod_roster. session.roster is guaranteed to be available for "c2s" session type.

--- plugins/mod_roster.lua
+++ plugins/mod_roster.lua
@@ -5,14 +5,6 @@ local send = require "core.sessionmanager".send_to_session
 add_iq_handler("c2s", "jabber:iq:roster", 
 		function (session, stanza)
 			if stanza.attr.type == "get" then
-				session.roster = session.roster or rostermanager.getroster(session.username, session.host);
-				if session.roster == false then
-					send(session, st.reply(stanza)
-						:tag("error", { type = "wait" })
-						:tag("internal-server-error", { xmlns = "urn:ietf:params:xml:ns:xmpp-stanzas"}));
-					return true;
-				else session.roster = session.roster or {};
-				end
 				local roster = st.reply(stanza)
 							:query("jabber:iq:roster");
 				for jid in pairs(session.roster) do
