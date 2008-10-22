ee7b432ab1d450a42fae24b37c79b7fbda7f4e6b
Matthew Wild <mwild1@gmail.com>
2008-10-22 17:36:21 +0100

    Abstract connections with "connection listeners" - Added connlistener for xmppclient - SASL/TLS now use a new session:reset_stream() method - main.lua on its way to being a bit neater

--- main.lua
+++ main.lua
@@ -1,9 +1,9 @@
 require "luarocks.require"
 
 local server = require "net.server"
+require "lxp"
 require "socket"
 require "ssl"
-require "lxp"
 
 function log(type, area, message)
 	print(type, area, message);
@@ -11,7 +11,10 @@ end
 
 dofile "lxmppd.cfg"
  
+-- Maps connections to sessions --
 sessions = {};
+ 
+-- Load and initialise core modules --
  
 require "util.import"
 require "core.stanza_dispatch"
@@ -22,10 +25,12 @@ require "core.modulemanager"
 require "core.usermanager"
 require "core.sessionmanager"
 require "core.stanza_router"
-require "net.connhandlers"
+
+local start = require "net.connlisteners".start;
 require "util.stanza"
 require "util.jid"
 
+------------------------------------------------------------------------
  
 -- Locals for faster access --
 local t_insert = table.insert;
@@ -37,62 +42,9 @@ local sm_new_session, sm_destroy_session = sessionmanager.new_session, sessionma
 local st = stanza;
 ------------------------------
 
-
-
 local hosts, sessions = hosts, sessions;
 
-function connect_host(host)
-	hosts[host] = { type = "remote", sendbuffer = {} };
-end
-
-function handler(conn, data, err)
-	local session = sessions[conn];
-
-	if not session then
-		sessions[conn] = sm_new_session(conn);
-		session = sessions[conn];
-
-		-- Logging functions --
-
-		local mainlog, log = log;
-		do
-			local conn_name = tostring(conn):match("%w+$");
-			log = function (type, area, message) mainlog(type, conn_name, message); end
-			--log = function () end
-		end
-		local print = function (...) log("info", "core", t_concatall({...}, "\t")); end
-		session.log = log;
-
-		print("Client connected");
-		
-		session.stanza_dispatch = function (stanza) return core_process_stanza(session, stanza); end
-		
-		session.connhandler = connhandlers.new("xmpp-client", session);
-			
-		function session.disconnect(err)
-			if session.last_presence and session.last_presence.attr.type ~= "unavailable" then
-				local pres = st.presence{ type = "unavailable" };
-				if err == "closed" then err = "connection closed"; end
-				pres:tag("status"):text("Disconnected: "..err);
-				session.stanza_dispatch(pres);
-			end
-			session = nil;
-			print("Disconnected: "..tostring(err));
-			collectgarbage("collect");
-		end
-	end
-	if data then
-		session.connhandler:data(data);
-	end
-	
-	--log("info", "core", "Client disconnected, connection closed");
-end
-
-function disconnect(conn, err)
-	sm_destroy_session(sessions[conn]);
-	sessions[conn] = nil;
-end
-
+-- Initialise modules
 modulemanager.loadall();
 
 setmetatable(_G, { __index = function (t, k) print("WARNING: ATTEMPT TO READ A NIL GLOBAL!!!", k); error("Attempt to read a non-existent global. Naughty boy.", 2); end, __newindex = function (t, k, v) print("ATTEMPT TO SET A GLOBAL!!!!", tostring(k).." = "..tostring(v)); error("Attempt to set a global. Naughty boy.", 2); end }) --]][][[]][];
@@ -101,7 +53,6 @@ setmetatable(_G, { __index = function (t, k) print("WARNING: ATTEMPT TO READ A N
 local protected_handler = function (conn, data, err) local success, ret = pcall(handler, conn, data, err); if not success then print("ERROR on "..tostring(conn)..": "..ret); conn:close(); end end;
 local protected_disconnect = function (conn, err) local success, ret = pcall(disconnect, conn, err); if not success then print("ERROR on "..tostring(conn).." disconnect: "..ret); conn:close(); end end;
 
-server.add( { listener = protected_handler, disconnect = protected_disconnect }, 5222, "*", 1, ssl_ctx ) -- server.add will send a status message
---server.add( { listener = protected_handler, disconnect = protected_disconnect }, 5223, "*", 1, ssl_ctx ) -- server.add will send a status message
+start("xmppclient", { ssl = ssl_ctx })
 
 server.loop();
--- plugins/mod_tls.lua
+++ plugins/mod_tls.lua
@@ -3,6 +3,8 @@ local st = require "util.stanza";
 local send = require "core.sessionmanager".send_to_session;
 local sm_bind_resource = require "core.sessionmanager".bind_resource;
 
+local sessions = sessions;
+
 local usermanager_validate_credentials = require "core.usermanager".validate_credentials;
 local t_concat, t_insert = table.concat, table.insert;
 local tostring = tostring;
@@ -16,16 +18,15 @@ local new_connhandler = require "net.connhandlers".new;
 add_handler("c2s_unauthed", "starttls", xmlns_starttls,
 		function (session, stanza)
 			if session.conn.starttls then
-				print("Wants to do TLS...");
 				send(session, st.stanza("proceed", { xmlns = xmlns_starttls }));
-				session.connhandler = new_connhandler("xmpp-client", session);
-				session.notopen = true;
-				if session.conn.starttls() then
-					print("Done");
-				else
-					print("Failed");
-				end
-				
+				-- FIXME: I'm commenting the below, not sure why it was necessary
+				-- sessions[session.conn] = nil;
+				session:reset_stream();
+				session.conn.starttls();
+				session.log("info", "TLS negotiation started...");
+			else
+				-- FIXME: What reply?
+				session.log("warn", "Attempt to start TLS, but TLS is not available on this connection");
 			end
 		end);
 		
