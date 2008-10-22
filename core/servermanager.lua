dffccde8c2a3cfef6353400ecb15572022704a6e
Waqas Hussain <waqas20@gmail.com>
2008-10-22 21:20:29 +0500

    Fixed: Unhandled stanza handling

--- core/servermanager.lua
+++ core/servermanager.lua
@@ -10,11 +10,13 @@ function handle_stanza(origin, stanza)
 	-- Use plugins
 	if not modulemanager.handle_stanza(origin, stanza) then
 		if stanza.name == "iq" then
-			local reply = st.reply(stanza);
-			reply.attr.type = "error";
-			reply:tag("error", { type = "cancel" })
-				:tag("service-unavailable", { xmlns = xmlns_stanzas });
-			send(origin, reply);
+			if stanza.attr.type ~= "result" and stanza.attr.type ~= "error" then
+				send(origin, st.error_reply(stanza, "cancel", "service-unavailable"));
+			end
+		elseif stanza.name == "message" then
+			send(origin, st.error_reply(stanza, "cancel", "service-unavailable"));
+		elseif stanza.name ~= "presence" then
+			error("Unknown stanza");
 		end
 	end
 end
