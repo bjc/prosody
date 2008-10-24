5c90ffdf7e7c7d784ed5f60eed3f8a2666c117f8
Matthew Wild <mwild1@gmail.com>
2008-10-24 07:27:36 +0100

    s2s works! \o/ \o/

---
+++ core/s2smanager.lua
@@ -0,0 +1,178 @@
+
+local hosts = hosts;
+local sessions = sessions;
+local socket = require "socket";
+local format = string.format;
+local tostring, pairs, ipairs, getmetatable, print, newproxy, error, tonumber
+    = tostring, pairs, ipairs, getmetatable, print, newproxy, error, tonumber;
+
+local connlisteners_get = require "net.connlisteners".get;
+local wraptlsclient = require "net.server".wraptlsclient;
+local modulemanager = require "core.modulemanager";
+
+local uuid_gen = require "util.uuid".generate;
+
+local logger_init = require "util.logger".init;
+
+local log = logger_init("s2smanager");
+
+local md5_hash = require "util.hashes".md5;
+
+local dialback_secret = "This is very secret!!! Ha!";
+
+module "s2smanager"
+
+function connect_host(from_host, to_host)
+end
+
+function send_to_host(from_host, to_host, data)
+	if hosts[to_host] then
+		-- Write to connection
+		hosts[to_host].send(data);
+		log("debug", "stanza sent over s2s");
+	else
+		log("debug", "opening a new outgoing connection for this stanza");
+		local host_session = new_outgoing(from_host, to_host);
+		-- Store in buffer
+		host_session.sendq = { data };
+	end
+end
+
+function disconnect_host(host)
+	
+end
+
+local open_sessions = 0;
+
+function new_incoming(conn)
+	local session = { conn = conn,  priority = 0, type = "s2sin_unauthed", direction = "incoming" };
+	if true then
+		session.trace = newproxy(true);
+		getmetatable(session.trace).__gc = function () open_sessions = open_sessions - 1; print("s2s session got collected, now "..open_sessions.." s2s sessions are allocated") end;
+	end
+	open_sessions = open_sessions + 1;
+	local w = conn.write;
+	session.send = function (t) w(tostring(t)); end
+	return session;
+end
+
+function new_outgoing(from_host, to_host)
+		local host_session = { to_host = to_host, from_host = from_host, notopen = true, type = "s2sout_unauthed", direction = "outgoing" };
+		hosts[to_host] = host_session;
+
+		local cl = connlisteners_get("xmppserver");
+		
+		local conn, handler = socket.tcp()
+		--FIXME: Below parameters (ports/ip) are incorrect (use SRV)
+		conn:connect(to_host, 5269);
+		conn = wraptlsclient(cl, conn, to_host, 5269, 0, 1, hosts[from_host].ssl_ctx );
+		host_session.conn = conn;
+		
+		-- Register this outgoing connection so that xmppserver_listener knows about it
+		-- otherwise it will assume it is a new incoming connection
+		cl.register_outgoing(conn, host_session);
+		
+		do
+			local conn_name = "s2sout"..tostring(conn):match("[a-f0-9]*$");
+			host_session.log = logger_init(conn_name);
+		end
+		
+		local w = conn.write;
+		host_session.send = function (t) w(tostring(t)); end
+		
+		conn.write(format([[<stream:stream xmlns='jabber:server' xmlns:db='jabber:server:dialback' xmlns:stream='http://etherx.jabber.org/streams' from='%s' to='%s' version='1.0'>]], from_host, to_host));
+		 
+		return host_session;
+end
+
+function streamopened(session, attr)
+	session.log("debug", "s2s stream opened");
+	local send = session.send;
+	
+	session.version = tonumber(attr.version) or 0;
+	if session.version >= 1.0 and not (attr.to and attr.from) then
+		print("to: "..tostring(attr.to).." from: "..tostring(attr.from));
+		--error(session.to_host.." failed to specify 'to' or 'from' hostname as per RFC");
+		log("warn", (session.to_host or "(unknown)").." failed to specify 'to' or 'from' hostname as per RFC");
+	end
+	
+	if session.direction == "incoming" then
+		-- Send a reply stream header
+		
+		for k,v in pairs(attr) do print("", tostring(k), ":::", tostring(v)); end
+		
+		session.to_host = attr.to;
+		session.from_host = attr.from;
+	
+		session.streamid = uuid_gen();
+		print(session, session.from_host, "incoming s2s stream opened");
+		send("<?xml version='1.0'?>");
+		send(format("<stream:stream xmlns='jabber:server' xmlns:db='jabber:server:dialback' xmlns:stream='http://etherx.jabber.org/streams' id='%s' from='%s'>", session.streamid, session.to_host));
+		if session.from_host then
+			-- Need to perform dialback to check identity
+			print("to: "..tostring(attr.to).." from: "..tostring(attr.from));
+			print("Need to do dialback here you know!!");
+		end
+	elseif session.direction == "outgoing" then
+		-- If we are just using the connection for verifying dialback keys, we won't try and auth it
+		if not session.dialback_verifying then
+			-- generate dialback key
+			if not attr.id then error("stream response did not give us a streamid!!!"); end
+			session.streamid = attr.id;
+			session.dialback_key = generate_dialback(session.streamid, session.to_host, session.from_host);
+			session.send(format("<db:result from='%s' to='%s'>%s</db:result>", session.from_host, session.to_host, session.dialback_key));
+			session.log("info", "sent dialback key on outgoing s2s stream");
+		else
+			mark_connected(session);
+		end
+	end
+	--[[
+	local features = {};
+	modulemanager.fire_event("stream-features-s2s", session, features);
+	
+	send("<stream:features>");
+	
+	for _, feature in ipairs(features) do
+		send(tostring(feature));
+	end
+
+	send("</stream:features>");]]
+	log("info", "s2s stream opened successfully");
+	session.notopen = nil;
+end
+
+function generate_dialback(id, to, from)
+	return md5_hash(id..to..from..dialback_secret); -- FIXME: See XEP-185 and XEP-220
+end
+
+function verify_dialback(id, to, from, key)
+	return key == generate_dialback(id, to, from);
+end
+
+function make_authenticated(session)
+	if session.type == "s2sout_unauthed" then
+		session.type = "s2sout";
+	elseif session.type == "s2sin_unauthed" then
+		session.type = "s2sin";
+	else
+		return false;
+	end
+	session.log("info", "connection is now authenticated");
+	
+	mark_connected(session);
+	
+	return true;
+end
+
+function mark_connected(session)
+	local sendq, send = session.sendq, session.send;
+	if sendq then
+		session.log("debug", "sending queued stanzas across new connection");
+		for _, data in ipairs(sendq) do
+			session.log("debug", "sending: %s", tostring(data));
+			send(data);
+		end
+	end
+end
+
+return _M;
--- net/xmppserver_listener.lua
+++ net/xmppserver_listener.lua
@@ -3,6 +3,8 @@ local logger = require "logger";
 local lxp = require "lxp"
 local init_xmlhandlers = require "core.xmlhandlers"
 local sm_new_session = require "core.sessionmanager".new_session;
+local s2s_new_incoming = require "core.s2smanager".new_incoming;
+local s2s_streamopened = require "core.s2smanager".streamopened;
 
 local connlisteners_register = require "net.connlisteners".register;
 
@@ -12,17 +14,16 @@ local t_concatall = function (t, sep) local tt = {}; for _, s in ipairs(t) do t_
 local m_random = math.random;
 local format = string.format;
 local sm_new_session, sm_destroy_session = sessionmanager.new_session, sessionmanager.destroy_session; --import("core.sessionmanager", "new_session", "destroy_session");
-local sm_streamopened = sessionmanager.streamopened;
 local st = stanza;
 
 local sessions = {};
-local xmppclient = { default_port = 5222 };
+local xmppserver = { default_port = 5269 };
 
 -- These are session methods --
 
 local function session_reset_stream(session)
 	-- Reset stream
-		local parser = lxp.new(init_xmlhandlers(session, sm_streamopened), "|");
+		local parser = lxp.new(init_xmlhandlers(session, s2s_streamopened), "|");
 		session.parser = parser;
 		
 		session.notopen = true;
@@ -35,29 +36,29 @@ end
 
 -- End of session methods --
 
-function xmppclient.listener(conn, data)
+function xmppserver.listener(conn, data)
 	local session = sessions[conn];
 	if not session then
-		session = sm_new_session(conn);
+		session = s2s_new_incoming(conn);
 		sessions[conn] = session;
 
 		-- Logging functions --
 
 		local mainlog, log = log;
 		do
-			local conn_name = tostring(conn):match("[a-f0-9]+$");
+			local conn_name = "s2sin"..tostring(conn):match("[a-f0-9]+$");
 			log = logger.init(conn_name);
 		end
 		local print = function (...) log("info", t_concatall({...}, "\t")); end
 		session.log = log;
 
-		print("Client connected");
+		print("Incoming s2s connection");
 		
 		session.reset_stream = session_reset_stream;
 		
 		session_reset_stream(session); -- Initialise, ready for use
 		
-		-- TODO: Below function should be session,stanza - and xmlhandlers should use :method() notation to call,
+		-- FIXME: Below function should be session,stanza - and xmlhandlers should use :method() notation to call,
 		-- this will avoid the useless indirection we have atm
 		-- (I'm on a mission, no time to fix now)
 		session.stanza_dispatch = function (stanza) return core_process_stanza(session, stanza); end
@@ -68,20 +69,27 @@ function xmppclient.listener(conn, data)
 	end
 end
 	
-function xmppclient.disconnect(conn)
-	local session = sessions[conn];
-	if session then
-		if session.last_presence and session.last_presence.attr.type ~= "unavailable" then
-			local pres = st.presence{ type = "unavailable" };
-			if err == "closed" then err = "connection closed"; end
-			pres:tag("status"):text("Disconnected: "..err);
-			session.stanza_dispatch(pres);
-		end
-		sm_destroy_session(session);
-		sessions[conn]  = nil;
-		session = nil;
-		collectgarbage("collect");
-	end
+function xmppserver.disconnect(conn)
 end
 
-connlisteners_register("xmppclient", xmppclient);
+function xmppserver.register_outgoing(conn, session)
+	session.direction = "outgoing";
+	sessions[conn] = session;
+	
+	session.reset_stream = session_reset_stream;	
+	session_reset_stream(session); -- Initialise, ready for use
+	
+	-- FIXME: Below function should be session,stanza - and xmlhandlers should use :method() notation to call,
+	-- this will avoid the useless indirection we have atm
+	-- (I'm on a mission, no time to fix now)
+	session.stanza_dispatch = function (stanza) return core_process_stanza(session, stanza); end
+end
+
+connlisteners_register("xmppserver", xmppserver);
+
+
+-- We need to perform some initialisation when a connection is created
+-- We also need to perform that same initialisation at other points (SASL, TLS, ...)
+
+-- ...and we need to handle data
+-- ...and record all sessions associated with connections
