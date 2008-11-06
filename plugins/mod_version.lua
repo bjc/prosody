a88ddeb67d6d8a31da4ac79d1c2d1a1d776c2c7c
Waqas Hussain <waqas20@gmail.com>
2008-11-06 16:57:10 +0500

    Fixed some whitespace.

--- plugins/mod_version.lua
+++ plugins/mod_version.lua
@@ -7,13 +7,12 @@ local log = require "util.logger".init("mod_version");
 local xmlns_version = "jabber:iq:version"
 
 local function handle_version_request(session, stanza)
-			if stanza.attr.type == "get" then
-				session.send(st.reply(stanza)
-								:query(xmlns_version)
-									:tag("name"):text("lxmppd"):up()
-									:tag("version"):text("pre-alpha"):up()
-									:tag("os"):text("The best one"));
-			end
+	if stanza.attr.type == "get" then
+		session.send(st.reply(stanza):query(xmlns_version)
+			:tag("name"):text("lxmppd"):up()
+			:tag("version"):text("pre-alpha"):up()
+			:tag("os"):text("The best one"));
+	end
 end
 
 add_iq_handler("c2s", xmlns_version, handle_version_request);
