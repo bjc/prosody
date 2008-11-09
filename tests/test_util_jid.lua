dfe7f483e474b7f37a577904c318b4389bd1fd90
Matthew Wild <mwild1@gmail.com>
2008-11-09 23:58:19 +0000

    Fix jid.split test function

--- tests/test_util_jid.lua
+++ tests/test_util_jid.lua
@@ -2,12 +2,13 @@
 function split(split)
 	function test(jid, node, server, resource)
 		local rnode, rserver, rresource = split(jid);
-		assert_equal(node, rnode, "split("..jid..") failed");
-		assert_equal(server, rserver, "split("..jid..") failed");
-		assert_equal(resource, rresource, "split("..jid..") failed");
+		assert_equal(node, rnode, "split("..tostring(jid)..") failed");
+		assert_equal(server, rserver, "split("..tostring(jid)..") failed");
+		assert_equal(resource, rresource, "split("..tostring(jid)..") failed");
 	end
 	test("node@server", 		"node", "server", nil		);
 	test("node@server/resource", 	"node", "server", "resource"	);
 	test("server", 			nil, 	"server", nil		);
 	test("server/resource", 	nil, 	"server", "resource"	);
+	test(nil,			nil,	nil	, nil		);
 end
