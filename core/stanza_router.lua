4f763680f03e927394c5f6f29d23081337c5e790
Waqas Hussain <waqas20@gmail.com>
2008-11-10 01:30:52 +0500

    Hack to allow s2s stanzas to work until we do proper namespace handling

--- core/stanza_router.lua
+++ core/stanza_router.lua
@@ -33,6 +33,7 @@ local print = print;
 function core_process_stanza(origin, stanza)
 	log("debug", "Received["..origin.type.."]: "..tostring(st.reply(st.reply(stanza))))
 
+	if not stanza.attr.xmlns then stanza.attr.xmlns = "jabber:client"; end -- FIXME Hack. This should be removed when we fix namespace handling.
 	-- TODO verify validity of stanza (as well as JID validity)
 	if stanza.name == "iq" and not(#stanza.tags == 1 and stanza.tags[1].attr.xmlns) then
 		if stanza.attr.type == "set" or stanza.attr.type == "get" then
