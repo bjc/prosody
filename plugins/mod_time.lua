5eafb3d324dc2aad0c35e8d7e8ded647175b2e90
Waqas Hussain <waqas20@gmail.com>
2008-11-26 08:56:30 +0500

    Added mod_time with support for [XEP-0202: Entity Time] and [XEP-0090: Entity Time] (deprecated)

---
+++ plugins/mod_time.lua
@@ -0,0 +1,28 @@
+
+local st = require "util.stanza";
+local datetime = require "util.datetime".datetime;
+
+-- XEP-0202: Entity Time
+
+require "core.discomanager".set("time", "urn:xmpp:time");
+
+add_iq_handler({"c2s", "s2sin"}, "urn:xmpp:time",
+	function(session, stanza)
+		if stanza.attr.type == "get" then
+			session.send(st.reply(stanza):tag("time", {xmlns="urn:xmpp:time"})
+				:tag("tzo"):text("+00:00"):up() -- FIXME get the timezone in a platform independent fashion
+				:tag("utc"):text(datetime()));
+		end
+	end);
+
+-- XEP-0090: Entity Time (deprecated)
+
+require "core.discomanager".set("time", "jabber:iq:time");
+
+add_iq_handler({"c2s", "s2sin"}, "jabber:iq:time",
+	function(session, stanza)
+		if stanza.attr.type == "get" then
+			session.send(st.reply(stanza):tag("query", {xmlns="jabber:iq:time"})
+				:tag("utc"):text(datetime()));
+		end
+	end);
