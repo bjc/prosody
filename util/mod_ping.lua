1ddc08657104d5c740cd61d9aa3bc276851e7c22
Waqas Hussain <waqas20@gmail.com>
2008-11-26 09:06:32 +0500

    Added mod_ping with support for [XEP-0199: XMPP Ping]

---
+++ util/mod_ping.lua
@@ -0,0 +1,11 @@
+
+local st = require "util.stanza";
+
+require "core.discomanager".set("ping", "urn:xmpp:ping");
+
+add_iq_handler({"c2s", "s2sin"}, "urn:xmpp:ping",
+	function(session, stanza)
+		if stanza.attr.type == "get" then
+			session.send(st.reply(stanza));
+		end
+	end);
