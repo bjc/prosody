533449b6d26621a0e410e84003f1902e319bbfb6
Matthew Wild <mwild1@gmail.com>
2008-12-04 17:10:02 +0000

    Don't send stream:features to incoming s2s connections

--- core/s2smanager.lua
+++ core/s2smanager.lua
@@ -180,7 +180,9 @@ end
 function streamopened(session, attr)
 	local send = session.sends2s;
 	
-	session.version = tonumber(attr.version) or 0;
+	-- TODO: #29: SASL/TLS on s2s streams
+	session.version = 0; --tonumber(attr.version) or 0;
+	
 	if session.version >= 1.0 and not (attr.to and attr.from) then
 		--print("to: "..tostring(attr.to).." from: "..tostring(attr.from));
 		log("warn", (session.to_host or "(unknown)").." failed to specify 'to' or 'from' hostname as per RFC");
