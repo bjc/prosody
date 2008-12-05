c016526d9d5b57c8b6157cba7eee4bb268e1e9b4
Matthew Wild <mwild1@gmail.com>
2008-12-05 19:49:16 +0000

    Backed out changeset 099d8a102deb (committed too much)

--- core/sessionmanager.lua
+++ core/sessionmanager.lua
@@ -35,8 +35,6 @@ local uuid_generate = require "util.uuid".generate;
 local rm_load_roster = require "core.rostermanager".load_roster;
 local config_get = require "core.configmanager".get;
 
-local gettime = require "socket".gettime;
-
 local st = require "util.stanza";
 
 local newproxy = newproxy;
@@ -47,7 +45,7 @@ module "sessionmanager"
 local open_sessions = 0;
 
 function new_session(conn)
-	local session = { conn = conn,  priority = 0, type = "c2s_unauthed", conntime = gettime() };
+	local session = { conn = conn,  priority = 0, type = "c2s_unauthed" };
 	if true then
 		session.trace = newproxy(true);
 		getmetatable(session.trace).__gc = function () open_sessions = open_sessions - 1; print("Session got collected, now "..open_sessions.." sessions are allocated") end;
@@ -111,8 +109,6 @@ function bind_resource(session, resource)
 	if session.resource then return nil, "cancel", "already-bound", "Cannot bind multiple resources on a single connection"; end
 	-- We don't support binding multiple resources
 
-	session.conntimetotal = gettime()-session.conntime;
-	
 	resource = resource or uuid_generate();
 	--FIXME: Randomly-generated resources must be unique per-user, and never conflict with existing
 	
