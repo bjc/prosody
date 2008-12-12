833006543a919f4d316f85d887e264cc15d65e2a
Matthew Wild <mwild1@gmail.com>
2008-12-12 04:06:15 +0000

    Non-blocking HTTP requests (adding net.http)

---
+++ net/httpclient_listener.lua
@@ -0,0 +1,36 @@
+
+local connlisteners_register = require "net.connlisteners".register;
+
+
+local requests = {}; -- Open requests
+local buffers = {}; -- Buffers of partial lines
+
+local httpclient = { default_port = 80, default_mode = "*a" };
+
+function httpclient.listener(conn, data)
+	local request = requests[conn];
+
+	if not request then
+		print("NO REQUEST!! for "..tostring(conn));
+		return;
+	end
+
+	if data and request.reader then
+		request:reader(data);
+	end
+end
+
+function httpclient.disconnect(conn, err)
+	local request = requests[conn];
+	if request then
+		request:reader(nil);
+	end
+	--requests[conn] = nil;
+end
+
+function httpclient.register_request(conn, req)
+	print("Registering a request for "..tostring(conn));
+	requests[conn] = req;
+end
+
+connlisteners_register("httpclient", httpclient);
