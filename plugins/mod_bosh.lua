236de71fea7eef294b75a42acbbf5b34de9efff5
Matthew Wild <mwild1@gmail.com>
2009-01-09 16:33:09 +0000

    Numerous BOSH improvements... handle client disconnects, either explicit or implicit through inactivity; allow specifying BOSH default parameters through config; fix to prevent prematurely closing request connections in some cases, before they were replied to

--- plugins/mod_bosh.lua
+++ plugins/mod_bosh.lua
@@ -6,27 +6,29 @@ local init_xmlhandlers = require "core.xmlhandlers"
 local server = require "net.server";
 local httpserver = require "net.httpserver";
 local sm = require "core.sessionmanager";
+local sm_destroy_session = sm.destroy_session;
 local new_uuid = require "util.uuid".generate;
 local fire_event = require "core.eventmanager".fire_event;
 local core_process_stanza = core_process_stanza;
 local st = require "util.stanza";
 local log = require "util.logger".init("bosh");
 local stream_callbacks = { stream_tag = "http://jabber.org/protocol/httpbind|body" };
-
+local config = require "core.configmanager";
 local xmlns_bosh = "http://jabber.org/protocol/httpbind"; -- (hard-coded into a literal in session.send)
 
-local BOSH_DEFAULT_HOLD = 1;
-local BOSH_DEFAULT_INACTIVITY = 30;
-local BOSH_DEFAULT_POLLING = 5;
-local BOSH_DEFAULT_REQUESTS = 2;
-local BOSH_DEFAULT_MAXPAUSE = 120;
+local BOSH_DEFAULT_HOLD = tonumber(config.get("*", "core", "bosh_default_hold")) or 1;
+local BOSH_DEFAULT_INACTIVITY = tonumber(config.get("*", "core", "bosh_max_inactivity")) or 30;
+local BOSH_DEFAULT_POLLING = tonumber(config.get("*", "core", "bosh_max_polling")) or 5;
+local BOSH_DEFAULT_REQUESTS = tonumber(config.get("*", "core", "bosh_max_requests")) or 2;
+local BOSH_DEFAULT_MAXPAUSE = tonumber(config.get("*", "core", "bosh_max_pause")) or 300;
 
 local t_insert, t_remove, t_concat = table.insert, table.remove, table.concat;
 local os_time = os.time;
 
 local sessions = {};
+local inactive_sessions = {}; -- Sessions which have no open requests
 
--- Used to respond to idle sessions
+-- Used to respond to idle sessions (those with waiting requests)
 local waiting_requests = {};
 function on_destroy_request(request)
 	waiting_requests[request] = nil;
@@ -34,7 +36,6 @@ end
 
 function handle_request(method, body, request)
 	if (not body) or request.method ~= "POST" then
-		--return { status = "200 OK", headers = { ["Content-Type"] = "text/html" }, body = "<html><body>You don't look like a BOSH client to me... what do you want?</body></html>" };
 		return "<html><body>You really don't look like a BOSH client to me... what do you want?</body></html>";
 	end
 	if not method then 
@@ -60,19 +61,16 @@ function handle_request(method, body, request)
 				log("debug", "...sending what is in the buffer")
 				session.send(t_concat(session.send_buffer));
 				session.send_buffer = {};
-				return;
 			else
 				-- or an empty response
 				log("debug", "...sending an empty response");
 				session.send("");
-				return;
 			end
 		elseif #session.send_buffer > 0 then
 			log("debug", "Session has data in the send buffer, will send now..");
 			local resp = t_concat(session.send_buffer);
 			session.send_buffer = {};
 			session.send(resp);
-			return;
 		end
 		
 		if not request.destroyed and session.bosh_wait then
@@ -86,6 +84,7 @@ function handle_request(method, body, request)
 	end
 end
 
+local session_close_reply = tostring(st.stanza("body", { xmlns = xmlns_bosh, type = "terminate" }));
 local function bosh_reset_stream(session) session.notopen = true; end
 local function bosh_close_stream(session, reason) end
 
@@ -95,14 +94,15 @@ function stream_callbacks.streamopened(request, attr)
 	log("debug", "BOSH body open (sid: %s)", attr.sid);
 	local sid = attr.sid
 	if not sid then
+		-- New session request
 		-- TODO: Sanity checks here (rid, to, known host, etc.)
 		request.notopen = nil; -- Signals that we accept this opening tag
 		
 		-- New session
 		sid = tostring(new_uuid());
 		local session = { type = "c2s_unauthed", conn = {}, sid = sid, rid = attr.rid, host = attr.to, bosh_version = attr.ver, bosh_wait = attr.wait, streamid = sid, 
-						bosh_hold = BOSH_DEFAULT_HOLD,
-						requests = { }, send_buffer = {}, reset_stream = bosh_reset_stream, close = bosh_close_stream };
+						bosh_hold = BOSH_DEFAULT_HOLD, bosh_max_inactive = BOSH_DEFAULT_INACTIVITY,
+						requests = { }, send_buffer = {}, reset_stream = bosh_reset_stream, close = bosh_close_stream, dispatch_stanza = core_process_stanza };
 		sessions[sid] = session;
 		log("info", "New BOSH session, assigned it sid '%s'", sid);
 		local r, send_buffer = session.requests, session.send_buffer;
@@ -133,6 +133,10 @@ function stream_callbacks.streamopened(request, attr)
 				end
 			elseif s ~= "" then
 				log("debug", "Saved to send buffer because there are %d open requests", #r);
+				if session.bosh_max_inactive and not inactive_sessions[session] then
+					inactive_sessions[session] = os_time();
+					(session.log or log)("debug", "BOSH session marked as inactive at %d", inactive_sessions[session]);
+				end
 				-- Hmm, no requests are open :(
 				t_insert(session.send_buffer, tostring(s));
 				log("debug", "There are now %d things in the send_buffer", #session.send_buffer);
@@ -145,7 +149,7 @@ function stream_callbacks.streamopened(request, attr)
 		fire_event("stream-features", session, features);
 		--xmpp:version='1.0' xmlns:xmpp='urn:xmpp:xbosh'
 		local response = st.stanza("body", { xmlns = xmlns_bosh, 
-									inactivity = "30", polling = "5", requests = "2", hold = tostring(session.bosh_hold), maxpause = "120", 
+									inactivity = tostring(BOSH_DEFAULT_INACTIVITY), polling = tostring(BOSH_DEFAULT_POLLING), requests = tostring(BOSH_DEFAULT_REQUESTS), hold = tostring(session.bosh_hold), maxpause = "120", 
 									sid = sid, ver  = '1.6', from = session.host, secure = 'true', ["xmpp:version"] = "1.0", 
 									["xmlns:xmpp"] = "urn:xmpp:xbosh", ["xmlns:stream"] = "http://etherx.jabber.org/streams" }):add_child(features);
 		request:send(tostring(response));
@@ -161,6 +165,25 @@ function stream_callbacks.streamopened(request, attr)
 		request:send(tostring(st.stanza("body", { xmlns = xmlns_bosh, type = "terminate", condition = "item-not-found" })));
 		request.notopen = nil;
 		return;
+	end
+	
+	if attr.type == "terminate" then
+		-- Client wants to end this session
+		(session.log or log)("info", "BOSH client disconnected");
+		for _, held_request in ipairs(session.requests) do
+				held_request:send(session_close_reply);
+				held_request:destroy();
+		end
+		sm_destroy_session(session);
+		sessions[sid]  = nil;
+		request.notopen = nil;
+		return;
+	end
+	
+	-- If session was inactive, make sure it is now marked as not
+	if #session.requests == 0 then
+		(session.log or log)("debug", "BOSH client now active again at %d", os_time());
+		inactive_sessions[session] = nil;
 	end
 	
 	if session.notopen then
@@ -198,6 +221,16 @@ function on_timer()
 			if request.conn then
 				sessions[request.sid].send("");
 			end
+		end
+	end
+	
+	now = now - 3;
+	for session, inactive_since in pairs(inactive_sessions) do
+		if now - inactive_since > session.bosh_max_inactive then
+			(session.log or log)("debug", "BOSH client inactive too long, destroying session at %d", now);
+			sessions[session.sid]  = nil;
+			inactive_sessions[session] = nil;
+			sm_destroy_session(session, "BOSH client silent for over "..session.bosh_max_inactive.." seconds");
 		end
 	end
 end
