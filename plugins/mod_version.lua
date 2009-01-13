c157d04ba36de495ca949fb72bcae323f0201bc7
Waqas Hussain <waqas20@gmail.com>
2009-01-13 19:18:37 +0500

    mod_version: Removed and rearranged some code

--- plugins/mod_version.lua
+++ plugins/mod_version.lua
@@ -21,20 +21,15 @@
 
 local st = require "util.stanza";
 
-local log = require "util.logger".init("mod_version");
-
 local xmlns_version = "jabber:iq:version"
 
 module:add_feature(xmlns_version);
 
-local function handle_version_request(session, stanza)
+module:add_iq_handler({"c2s", "s2sin"}, xmlns_version, function(session, stanza)
 	if stanza.attr.type == "get" then
 		session.send(st.reply(stanza):query(xmlns_version)
 			:tag("name"):text("Prosody"):up()
 			:tag("version"):text("0.2"):up()
 			:tag("os"):text("the best operating system ever!"));
 	end
-end
-
-module:add_iq_handler("c2s", xmlns_version, handle_version_request);
-module:add_iq_handler("s2sin", xmlns_version, handle_version_request);
+end);
