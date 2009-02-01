9d01e12f12fe253263e9434c92789dc10553a495
Matthew Wild <mwild1@gmail.com>
2009-02-01 02:20:11 +0000

    mod_bosh: No need to tostring() uuids now

--- plugins/mod_bosh.lua
+++ plugins/mod_bosh.lua
@@ -119,7 +119,7 @@ function stream_callbacks.streamopened(request, attr)
 		end
 		
 		-- New session
-		sid = tostring(new_uuid());
+		sid = new_uuid();
 		local session = { type = "c2s_unauthed", conn = {}, sid = sid, rid = attr.rid, host = attr.to, bosh_version = attr.ver, bosh_wait = attr.wait, streamid = sid, 
 						bosh_hold = BOSH_DEFAULT_HOLD, bosh_max_inactive = BOSH_DEFAULT_INACTIVITY,
 						requests = { }, send_buffer = {}, reset_stream = bosh_reset_stream, close = bosh_close_stream, dispatch_stanza = core_process_stanza };
