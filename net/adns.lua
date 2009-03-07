67722b4f90302383716a7ffe3b4bd4acf44cbb5f
Matthew Wild <mwild1@gmail.com>
2009-03-07 19:57:28 +0000

    net.adns: Set new send in net.server after 'connecting UDP' socket

--- net/adns.lua
+++ net/adns.lua
@@ -29,7 +29,7 @@ function new_async_socket(sock)
 	newconn.handler, newconn._socket = server.wrapclient(sock, "dns", 53, listener);
 	newconn.handler.settimeout = function () end
 	newconn.handler.setsockname = function (_, ...) return sock:setsockname(...); end
-	newconn.handler.setpeername = function (_, ...) return sock:setpeername(...); end
+	newconn.handler.setpeername = function (_, ...) local ret = sock:setpeername(...); _.setsend(sock.send); return ret; end
 	newconn.handler.connect = function (_, ...) return sock:connect(...) end	
 	newconn.handler.send = function (_, data) return _.write(data) end	
 	return newconn.handler;
