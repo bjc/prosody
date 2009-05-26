88cc36f4d891744ef730e4e46b9d276454f51d40
Matthew Wild <mwild1@gmail.com>
2009-05-26 04:51:05 +0100

    mod_watchregistrations: New plugin to send a message to admins when a new user registers

---
+++ plugins/mod_watchregistrations.lua
@@ -0,0 +1,25 @@
+
+local host = module:get_host();
+
+local config = require "core.configmanager";
+
+local registration_watchers = config.get(host, "core", "registration_watchers") 
+	or config.get(host, "core", "admin") or {};
+
+local registration_alert = config.get(host, "core", "registration_notification") or "User $username just registered on $host from $ip";
+
+local st = require "util.stanza";
+
+module:add_event_hook("user-registered", function (user)
+		module:log("debug", "Notifying of new registration");
+		local message = st.message{ type = "chat", from = host }
+					:tag("body")
+					:text(registration_alert:gsub("%$(%w+)", 
+						function (v) return user[v] or user.session and user.session[v] or nil; end));
+		
+		for _, jid in ipairs(registration_watchers) do
+			module:log("debug", "Notifying %s", jid);
+			message.attr.to = jid;
+			core_route_stanza(hosts[host], message);
+		end
+	end);
