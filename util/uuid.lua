43ef081e34bc38d0ab014f1b187bc0524424d3f0
Waqas Hussain <waqas20@gmail.com>
2009-06-04 17:41:55 +0500

    util.uuid: More uniqueness!

--- util/uuid.lua
+++ util/uuid.lua
@@ -7,13 +7,28 @@
 --
 
 
-
 local m_random = math.random;
 local tostring = tostring;
+local os_time = os.time;
+local os_clock = os.clock;
+local sha1 = require "util.hashes".sha1;
+
 module "uuid"
 
+local last_uniq_time = 0;
+local function uniq_time()
+	local new_uniq_time = os_time();
+	if last_uniq_time >= new_uniq_time then new_uniq_time = last_uniq_time + 1; end
+	last_uniq_time = new_uniq_time;
+	return new_uniq_time;
+end
+
+local function new_random(x)
+	return sha1(x..os_clock()..tostring({}), true);
+end
+
 function generate()
-	return tostring(m_random(0, 99999999));
+	return new_random(uniq_time());
 end
 
 return _M;
