93c807593ea1f48158a14537a5dd59eca1eec5ef
Waqas Hussain <waqas20@gmail.com>
2009-08-08 23:41:16 +0500

    mod_selftests: Updated to use module:get_option instead of configmanager

--- plugins/mod_selftests.lua
+++ plugins/mod_selftests.lua
@@ -12,8 +12,7 @@ local st = require "util.stanza";
 local register_component = require "core.componentmanager".register_component;
 local core_route_stanza = core_route_stanza;
 local socket = require "socket";
-local config = require "core.configmanager";
-local ping_hosts = config.get("*", "mod_selftests", "ping_hosts") or { "coversant.interop.xmpp.org", "djabberd.interop.xmpp.org", "djabberd-trunk.interop.xmpp.org", "ejabberd.interop.xmpp.org", "openfire.interop.xmpp.org" };
+local ping_hosts = module:get_option("ping_hosts") or { "coversant.interop.xmpp.org", "djabberd.interop.xmpp.org", "djabberd-trunk.interop.xmpp.org", "ejabberd.interop.xmpp.org", "openfire.interop.xmpp.org" };
 
 local open_pings = {};
 
