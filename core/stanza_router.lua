269a562f52e2d404c4d49122436029d07d17711c
Waqas Hussain <waqas20@gmail.com>
2009-10-04 21:46:35 +0500

    stanza_router: Reply to IQ requests with missing 'id' attribute with a bad-request error.

--- core/stanza_router.lua
+++ core/stanza_router.lua
@@ -26,8 +26,12 @@ function core_process_stanza(origin, stanza)
 	-- TODO verify validity of stanza (as well as JID validity)
 	if stanza.attr.type == "error" and #stanza.tags == 0 then return; end -- TODO invalid stanza, log
 	if stanza.name == "iq" then
-		if (stanza.attr.type == "set" or stanza.attr.type == "get") and #stanza.tags ~= 1 then
+		local can_reply = stanza.attr.type == "set" or stanza.attr.type == "get"
+		local missing_id = not stanza.attr.id;
+		if can_reply and (#stanza.tags ~= 1 or missing_id) then
 			origin.send(st.error_reply(stanza, "modify", "bad-request"));
+			return;
+		elseif missing_id then
 			return;
 		end
 	end
