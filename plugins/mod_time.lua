33fa5e21e7b24a60a7d1871bcaecb336de674a43
Waqas Hussain <waqas20@gmail.com>
2009-10-18 18:45:41 +0500

    mod_time: Updated to use events (which also fixes a few minor issues).

--- plugins/mod_time.lua
+++ plugins/mod_time.lua
@@ -6,8 +6,6 @@
 -- COPYING file in the source package for more information.
 --
 
-
-
 local st = require "util.stanza";
 local datetime = require "util.datetime".datetime;
 local legacy = require "util.datetime".legacy;
@@ -16,23 +14,31 @@ local legacy = require "util.datetime".legacy;
 
 module:add_feature("urn:xmpp:time");
 
-module:add_iq_handler({"c2s", "s2sin"}, "urn:xmpp:time",
-	function(session, stanza)
-		if stanza.attr.type == "get" then
-			session.send(st.reply(stanza):tag("time", {xmlns="urn:xmpp:time"})
-				:tag("tzo"):text("+00:00"):up() -- FIXME get the timezone in a platform independent fashion
-				:tag("utc"):text(datetime()));
-		end
-	end);
+local function time_handler(event)
+	local origin, stanza = event.origin, event.stanza;
+	if stanza.attr.type == "get" then
+		origin.send(st.reply(stanza):tag("time", {xmlns="urn:xmpp:time"})
+			:tag("tzo"):text("+00:00"):up() -- TODO get the timezone in a platform independent fashion
+			:tag("utc"):text(datetime()));
+		return true;
+	end
+end
+
+module:hook("iq/bare/urn:xmpp:time:time", time_handler);
+module:hook("iq/host/urn:xmpp:time:time", time_handler);
 
 -- XEP-0090: Entity Time (deprecated)
 
 module:add_feature("jabber:iq:time");
 
-module:add_iq_handler({"c2s", "s2sin"}, "jabber:iq:time",
-	function(session, stanza)
-		if stanza.attr.type == "get" then
-			session.send(st.reply(stanza):tag("query", {xmlns="jabber:iq:time"})
-				:tag("utc"):text(legacy()));
-		end
-	end);
+local function legacy_time_handler(event)
+	local origin, stanza = event.origin, event.stanza;
+	if stanza.attr.type == "get" then
+		origin.send(st.reply(stanza):tag("query", {xmlns="jabber:iq:time"})
+			:tag("utc"):text(legacy()));
+		return true;
+	end
+end
+
+module:hook("iq/bare/jabber:iq:time:query", legacy_time_handler);
+module:hook("iq/host/jabber:iq:time:query", legacy_time_handler);
