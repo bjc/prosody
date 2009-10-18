87c9d431dff65fc54a54528dcf591091a0e2963a
Waqas Hussain <waqas20@gmail.com>
2009-10-18 07:30:58 +0500

    mod_vcard: Refactoring and cleanup.

--- plugins/mod_vcard.lua
+++ plugins/mod_vcard.lua
@@ -6,8 +6,6 @@
 -- COPYING file in the source package for more information.
 --
 
-
-
 local hosts = _G.hosts;
 local datamanager = require "util.datamanager"
 
@@ -18,63 +16,56 @@ local jid = require "util.jid"
 local jid_split = jid.split;
 
 local xmlns_vcard = "vcard-temp";
-
 module:add_feature(xmlns_vcard);
 
 function handle_vcard(event)
 	local session, stanza = event.origin, event.stanza;
-	if stanza.tags[1].name == "vCard" then
-		local to = stanza.attr.to;
-		if stanza.attr.type == "get" then
-			local vCard;
-			if to then
-				local node, host = jid_split(to);
-				if hosts[host] and hosts[host].type == "local" then
-					vCard = st.deserialize(datamanager.load(node, host, "vcard")); -- load vCard for user or server
-				end
-			else
-				vCard = st.deserialize(datamanager.load(session.username, session.host, "vcard"));-- load user's own vCard
-			end
-			if vCard then
-				session.send(st.reply(stanza):add_child(vCard)); -- send vCard!
-			else
-				session.send(st.error_reply(stanza, "cancel", "item-not-found"));
-			end
-		elseif stanza.attr.type == "set" then
-			if not to then
-				if datamanager.store(session.username, session.host, "vcard", st.preserialize(stanza.tags[1])) then
-					session.send(st.reply(stanza));
-				else
-					-- TODO unable to write file, file may be locked, etc, what's the correct error?
-					session.send(st.error_reply(stanza, "wait", "internal-server-error"));
-				end
-			else
-				session.send(st.error_reply(stanza, "auth", "forbidden"));
-			end
+	local to = stanza.attr.to;
+	if stanza.attr.type == "get" then
+		local vCard;
+		if to then
+			local node, host = jid_split(to);
+			vCard = st.deserialize(datamanager.load(node, host, "vcard")); -- load vCard for user or server
+		else
+			vCard = st.deserialize(datamanager.load(session.username, session.host, "vcard"));-- load user's own vCard
 		end
-		return true;
+		if vCard then
+			session.send(st.reply(stanza):add_child(vCard)); -- send vCard!
+		else
+			session.send(st.error_reply(stanza, "cancel", "item-not-found"));
+		end
+	else
+		if not to then
+			if datamanager.store(session.username, session.host, "vcard", st.preserialize(stanza.tags[1])) then
+				session.send(st.reply(stanza));
+			else
+				-- TODO unable to write file, file may be locked, etc, what's the correct error?
+				session.send(st.error_reply(stanza, "wait", "internal-server-error"));
+			end
+		else
+			session.send(st.error_reply(stanza, "auth", "forbidden"));
+		end
 	end
+	return true;
 end
 
---module:add_iq_handler({"c2s", "s2sin", "component"}, xmlns_vcard, handle_vcard);
 module:hook("iq/bare/vcard-temp:vCard", handle_vcard);
 module:hook("iq/host/vcard-temp:vCard", handle_vcard);
 
 -- COMPAT: https://support.process-one.net/browse/EJAB-1045
 if module:get_option("vcard_compatibility") then
-	module:hook("iq/full", function (data)
+	module:hook("iq/full", function(data)
 		local stanza = data.stanza;
-		if stanza.attr.type == "get" and stanza.tags[1]
-			and stanza.tags[1].attr.xmlns == xmlns_vcard then
-				return handle_vcard(data);
+		local payload = stanza.tags[1];
+		if stanza.attr.type == "get" or stanza.attr.type == "set" and payload.name == "vCard" and payload.attr.xmlns == xmlns_vcard then
+			return handle_vcard(data);
 		end
 	end, 1);
 end
 
-local feature_vcard_attr = { var=xmlns_vcard };
-module:add_event_hook("stream-features",
-					function (session, features)
-						if session.type == "c2s" then
-							features:tag("feature", feature_vcard_attr):up();
-						end
-					end);
+local feature_vcard = st.stanza("feature", { var = xmlns_vcard });
+module:add_event_hook("stream-features", function(session, features)
+	if session.type == "c2s" then
+		features:add_child(feature_vcard);
+	end
+end);
