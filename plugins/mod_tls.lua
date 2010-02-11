16b4fa23d18cb061379f1c4a2c253e1220bb96dd
Waqas Hussain <waqas20@gmail.com>
2010-02-12 01:47:10 +0500

    mod_tls: Slight refactoring.

--- plugins/mod_tls.lua
+++ plugins/mod_tls.lua
@@ -16,35 +16,36 @@ local secure_s2s_only = module:get_option("s2s_require_encryption");
 
 local global_ssl_ctx = prosody.global_ssl_ctx;
 
-module:add_handler("c2s_unauthed", "starttls", xmlns_starttls,
-		function (session, stanza)
-			if session.conn.starttls then
-				session.send(st.stanza("proceed", { xmlns = xmlns_starttls }));
-				session:reset_stream();
-				local ssl_ctx = session.host and hosts[session.host].ssl_ctx_in or global_ssl_ctx;
-				session.conn:starttls(ssl_ctx);
-				session.log("info", "TLS negotiation started...");
-				session.secure = false;
-			else
-				-- FIXME: What reply?
-				session.log("warn", "Attempt to start TLS, but TLS is not available on this connection");
-			end
-		end);
-		
-module:add_handler("s2sin_unauthed", "starttls", xmlns_starttls,
-		function (session, stanza)
-			if session.conn.starttls then
-				session.sends2s(st.stanza("proceed", { xmlns = xmlns_starttls }));
-				session:reset_stream();
-				local ssl_ctx = session.to_host and hosts[session.to_host].ssl_ctx_in or global_ssl_ctx;
-				session.conn:starttls(ssl_ctx);
-				session.log("info", "TLS negotiation started for incoming s2s...");
-				session.secure = false;
-			else
-				-- FIXME: What reply?
-				session.log("warn", "Attempt to start TLS, but TLS is not available on this s2s connection");
-			end
-		end);
+function c2s_starttls_handler(session, stanza)
+	if session.conn.starttls then
+		session.send(st.stanza("proceed", { xmlns = xmlns_starttls }));
+		session:reset_stream();
+		local ssl_ctx = session.host and hosts[session.host].ssl_ctx_in or global_ssl_ctx;
+		session.conn:starttls(ssl_ctx);
+		session.log("info", "TLS negotiation started...");
+		session.secure = false;
+	else
+		-- FIXME: What reply?
+		session.log("warn", "Attempt to start TLS, but TLS is not available on this connection");
+	end
+end
+
+function s2s_starttls_handler(session, stanza)
+	if session.conn.starttls then
+		session.sends2s(st.stanza("proceed", { xmlns = xmlns_starttls }));
+		session:reset_stream();
+		local ssl_ctx = session.to_host and hosts[session.to_host].ssl_ctx_in or global_ssl_ctx;
+		session.conn:starttls(ssl_ctx);
+		session.log("info", "TLS negotiation started for incoming s2s...");
+		session.secure = false;
+	else
+		-- FIXME: What reply?
+		session.log("warn", "Attempt to start TLS, but TLS is not available on this s2s connection");
+	end
+end
+
+module:add_handler("c2s_unauthed", "starttls", xmlns_starttls, c2s_starttls_handler);
+module:add_handler("s2sin_unauthed", "starttls", xmlns_starttls, s2s_starttls_handler);
 
 
 local starttls_attr = { xmlns = xmlns_starttls };
