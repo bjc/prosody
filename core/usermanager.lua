226d0936b3ede43be6319474a9eb905dbe8fbc60
Waqas Hussain <waqas20@gmail.com>
2010-05-19 10:49:27 +0500

    usermanager: Changed auth provider functions to methods.

--- core/usermanager.lua
+++ core/usermanager.lua
@@ -44,7 +44,7 @@ local function is_cyrus(host) return config.get(host, "core", "sasl_backend") ==
 function new_default_provider(host)
 	local provider = { name = "default" };
 	
-	function provider.test_password(username, password)
+	function provider:test_password(username, password)
 		if is_cyrus(host) then return nil, "Legacy auth not supported with Cyrus SASL."; end
 		local credentials = datamanager.load(username, host, "accounts") or {};
 	
@@ -55,12 +55,12 @@ function new_default_provider(host)
 		end
 	end
 
-	function provider.get_password(username)
+	function provider:get_password(username)
 		if is_cyrus(host) then return nil, "Passwords unavailable for Cyrus SASL."; end
 		return (datamanager.load(username, host, "accounts") or {}).password;
 	end
 	
-	function provider.set_password(username, password)
+	function provider:set_password(username, password)
 		if is_cyrus(host) then return nil, "Passwords unavailable for Cyrus SASL."; end
 		local account = datamanager.load(username, host, "accounts");
 		if account then
@@ -70,21 +70,21 @@ function new_default_provider(host)
 		return nil, "Account not available.";
 	end
 
-	function provider.user_exists(username)
+	function provider:user_exists(username)
 		if is_cyrus(host) then return true; end
 		return datamanager.load(username, host, "accounts") ~= nil; -- FIXME also check for empty credentials
 	end
 
-	function provider.create_user(username, password)
+	function provider:create_user(username, password)
 		if is_cyrus(host) then return nil, "Account creation/modification not available with Cyrus SASL."; end
 		return datamanager.store(username, host, "accounts", {password = password});
 	end
 
-	function provider.get_supported_methods()
+	function provider:get_supported_methods()
 		return {["PLAIN"] = true, ["DIGEST-MD5"] = true}; -- TODO this should be taken from the config
 	end
 
-	function provider.is_admin(jid)
+	function provider:is_admin(jid)
 		local admins = config.get(host, "core", "admins");
 		if admins ~= config.get("*", "core", "admins") then
 			if type(admins) == "table" then
@@ -102,32 +102,32 @@ function new_default_provider(host)
 end
 
 function validate_credentials(host, username, password, method)
-	return hosts[host].users.test_password(username, password);
+	return hosts[host].users:test_password(username, password);
 end
 
 function get_password(username, host)
-	return hosts[host].users.get_password(username);
+	return hosts[host].users:get_password(username);
 end
 
 function set_password(username, host, password)
-	return hosts[host].users.set_password(username, password);
+	return hosts[host].users:set_password(username, password);
 end
 
 function user_exists(username, host)
-	return hosts[host].users.user_exists(username);
+	return hosts[host].users:user_exists(username);
 end
 
 function create_user(username, password, host)
-	return hosts[host].users.create_user(username, password);
+	return hosts[host].users:create_user(username, password);
 end
 
 function get_supported_methods(host)
-	return hosts[host].users.get_supported_methods();
+	return hosts[host].users:get_supported_methods();
 end
 
 function is_admin(jid, host)
 	if host and host ~= "*" then
-		return hosts[host].users.is_admin(jid);
+		return hosts[host].users:is_admin(jid);
 	else -- Test only whether this JID is a global admin
 		local admins = config.get("*", "core", "admins");
 		if type(admins) == "table" then
