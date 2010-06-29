05d8e845ca4a623e261297a9a19e6315aa7c1ef6
Matthew Wild <mwild1@gmail.com>
2010-06-29 20:21:17 +0100

    mod_register: Remove the offline store as a list (originally aa3725bbcde5 but missed in a merge)

--- plugins/mod_register.lua
+++ plugins/mod_register.lua
@@ -46,7 +46,7 @@ module:add_iq_handler("c2s", "jabber:iq:register", function (session, stanza)
 				-- TODO datamanager should be able to delete all user data itself
 				datamanager.store(username, host, "vcard", nil);
 				datamanager.store(username, host, "private", nil);
-				datamanager.store(username, host, "offline", nil);
+				datamanager.list_store(username, host, "offline", nil);
 				local bare = username.."@"..host;
 				for jid, item in pairs(roster) do
 					if jid and jid ~= "pending" then
