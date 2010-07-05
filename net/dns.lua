90b0ef76c687a4c819f9487a35f5f615d2a2f7db
Matthew Wild <mwild1@gmail.com>
2010-07-05 12:13:55 +0100

    net.dns: Add dns.settimeout() to set the timeout for the default resolver

--- net/dns.lua
+++ net/dns.lua
@@ -993,6 +993,10 @@ function dns.cancel(...)  -- - - - - - - - - - - - - - - - - - - - - -  cancel
 	return _resolver:cancel(...);
 end
 
+function dns.settimeout(...)
+	return _resolver:settimeout(...);
+end
+
 function dns.socket_wrapper_set(...)    -- - - - - - - - -  socket_wrapper_set
 	return _resolver:socket_wrapper_set(...);
 end
