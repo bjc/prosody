03543cb53ae0621ecce5edce18bbe91879a1143e
Florian Zeitz <florob@babelmonkeys.de>
2010-07-10 01:17:07 +0200

    mod_adhoc: Add capability to pass data to forms

--- plugins/adhoc/adhoc.lib.lua
+++ plugins/adhoc/adhoc.lib.lua
@@ -69,9 +69,9 @@ function _M.handle_cmd(command, origin, stanza)
 			end
 			cmdtag:add_child(actions);
 		elseif name == "form" then
-			cmdtag:add_child(content:form());
+			cmdtag:add_child((content.layout or content):form(content.data));
 		elseif name == "result" then
-			cmdtag:add_child(content.layout:form(content.data, "result"));
+			cmdtag:add_child((content.layout or content):form(content.data, "result"));
 		elseif name == "other" then
 			cmdtag:add_child(content);
 		end
