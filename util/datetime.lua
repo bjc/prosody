d6cf6f600495c810fddf4a06711fb46e6ce60bea
Waqas Hussain <waqas20@gmail.com>
2010-08-02 20:11:08 +0500

    util.datetime: Added implementation for function parse().

--- util/datetime.lua
+++ util/datetime.lua
@@ -10,7 +10,10 @@
 -- XEP-0082: XMPP Date and Time Profiles
 
 local os_date = os.date;
+local os_time = os.time;
+local os_difftime = os.difftime;
 local error = error;
+local tonumber = tonumber;
 
 module "datetime"
 
@@ -31,7 +34,23 @@ function legacy(t)
 end
 
 function parse(s)
-	error("datetime.parse: Not implemented"); -- TODO
+	if s then
+		local year, month, day, hour, min, sec, tzd;
+		year, month, day, hour, min, sec, tzd = s:match("^(%d%d%d%d)-?(%d%d)-?(%d%d)T(%d%d):(%d%d):(%d%d)%.?%d*([Z+%-].*)$");
+		if year then
+			local time_offset = os_difftime(os_time(os_date("*t")), os_time(os_date("!*t"))); -- to deal with local timezone
+			local tzd_offset = 0;
+			if tzd ~= "" and tzd ~= "Z" then
+				local sign, h, m = tzd:match("([+%-])(%d%d):(%d%d)");
+				if not sign then return; end
+				h, m = tonumber(h), tonumber(m);
+				tzd_offset = h * 60 * 60 + m * 60;
+				if sign == "-" then tzd_offset = -tzd_offset; end
+			end
+			sec = sec + time_offset + tzd_offset;
+			return os_time({year=year, month=month, day=day, hour=hour, min=min, sec=sec});
+		end
+	end
 end
 
 return _M;
