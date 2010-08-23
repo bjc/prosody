bdd2c3c7927e31676bcec6aea387cad26cb65921
Matthew Wild <mwild1@gmail.com>
2010-08-23 01:06:13 +0100

    net.server: Don't load configmanager if not running inside Prosody, assume server_select

--- net/server.lua
+++ net/server.lua
@@ -6,7 +6,7 @@
 -- COPYING file in the source package for more information.
 --
 
-local use_luaevent = require "core.configmanager".get("*", "core", "use_libevent");
+local use_luaevent = prosody and require "core.configmanager".get("*", "core", "use_libevent");
 
 if use_luaevent then
 	use_luaevent = pcall(require, "luaevent.core");
