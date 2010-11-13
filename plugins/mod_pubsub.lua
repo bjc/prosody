9d08cacdc7a15399eda029b050a3d5cc2d9ab773
Matthew Wild <mwild1@gmail.com>
2010-11-13 04:10:05 +0000

    mod_pubsub: Expose 'service'

--- plugins/mod_pubsub.lua
+++ plugins/mod_pubsub.lua
@@ -87,8 +87,10 @@ function simple_broadcast(node, jids, item)
 	end
 end
 
+module:hook("iq/host/http://jabber.org/protocol/pubsub:pubsub", handle_pubsub_iq);
+
 service = pubsub.new({
 	broadcaster = simple_broadcast
 });
+_M.service = service;
 
-module:hook("iq/host/http://jabber.org/protocol/pubsub:pubsub", handle_pubsub_iq);
