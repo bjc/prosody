67b4334c9b362fa1ee761629f3953c1553f5d3a1
Waqas Hussain <waqas20@gmail.com>
2010-11-28 02:03:59 +0500

    mod_iq: IQ error and result sub-events are now "iq-{error,result}/{host,self,bare}/id" (previously "iq/{host,self,bare}/id").

--- plugins/mod_iq.lua
+++ plugins/mod_iq.lua
@@ -13,66 +13,55 @@ local jid_split = require "util.jid".split;
 local full_sessions = full_sessions;
 local bare_sessions = bare_sessions;
 
-if module:get_host_type() == "local" then
-	module:hook("iq/full", function(data)
-		-- IQ to full JID recieved
-		local origin, stanza = data.origin, data.stanza;
+module:hook("iq/full", function(data)
+	-- IQ to full JID recieved
+	local origin, stanza = data.origin, data.stanza;
 
-		local session = full_sessions[stanza.attr.to];
-		if session then
-			-- TODO fire post processing event
-			session.send(stanza);
-		else -- resource not online
-			if stanza.attr.type == "get" or stanza.attr.type == "set" then
-				origin.send(st.error_reply(stanza, "cancel", "service-unavailable"));
-			end
+	local session = full_sessions[stanza.attr.to];
+	if session then
+		-- TODO fire post processing event
+		session.send(stanza);
+	else -- resource not online
+		if stanza.attr.type == "get" or stanza.attr.type == "set" then
+			origin.send(st.error_reply(stanza, "cancel", "service-unavailable"));
 		end
-		return true;
-	end);
-end
+	end
+	return true;
+end);
 
 module:hook("iq/bare", function(data)
 	-- IQ to bare JID recieved
 	local origin, stanza = data.origin, data.stanza;
-	local type = stanza.attr.type;
 
 	-- TODO fire post processing events
-	if type == "get" or type == "set" then
-		local child = stanza.tags[1];
-		local ret = module:fire_event("iq/bare/"..child.attr.xmlns..":"..child.name, data);
-		if ret ~= nil then return ret; end
-		return module:fire_event("iq-"..type.."/bare/"..child.attr.xmlns..":"..child.name, data);
+	if stanza.attr.type == "get" or stanza.attr.type == "set" then
+		return module:fire_event("iq/bare/"..stanza.tags[1].attr.xmlns..":"..stanza.tags[1].name, data);
 	else
-		return module:fire_event("iq-"..type.."/bare/"..stanza.attr.id, data);
+		module:fire_event("iq-"..stanza.attr.type.."/bare/"..stanza.attr.id, data);
+		return true;
 	end
 end);
 
 module:hook("iq/self", function(data)
-	-- IQ to self JID recieved
+	-- IQ to bare JID recieved
 	local origin, stanza = data.origin, data.stanza;
-	local type = stanza.attr.type;
 
-	if type == "get" or type == "set" then
-		local child = stanza.tags[1];
-		local ret = module:fire_event("iq/self/"..child.attr.xmlns..":"..child.name, data);
-		if ret ~= nil then return ret; end
-		return module:fire_event("iq-"..type.."/self/"..child.attr.xmlns..":"..child.name, data);
+	if stanza.attr.type == "get" or stanza.attr.type == "set" then
+		return module:fire_event("iq/self/"..stanza.tags[1].attr.xmlns..":"..stanza.tags[1].name, data);
 	else
-		return module:fire_event("iq-"..type.."/self/"..stanza.attr.id, data);
+		module:fire_event("iq-"..stanza.attr.type.."/self/"..stanza.attr.id, data);
+		return true;
 	end
 end);
 
 module:hook("iq/host", function(data)
 	-- IQ to a local host recieved
 	local origin, stanza = data.origin, data.stanza;
-	local type = stanza.attr.type;
 
-	if type == "get" or type == "set" then
-		local child = stanza.tags[1];
-		local ret = module:fire_event("iq/host/"..child.attr.xmlns..":"..child.name, data);
-		if ret ~= nil then return ret; end
-		return module:fire_event("iq-"..type.."/host/"..child.attr.xmlns..":"..child.name, data);
+	if stanza.attr.type == "get" or stanza.attr.type == "set" then
+		return module:fire_event("iq/host/"..stanza.tags[1].attr.xmlns..":"..stanza.tags[1].name, data);
 	else
-		return module:fire_event("iq-"..type.."/host/"..stanza.attr.id, data);
+		module:fire_event("iq-"..stanza.attr.type.."/host/"..stanza.attr.id, data);
+		return true;
 	end
 end);
