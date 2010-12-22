79f8d774812bc98bd432e5341814446100e24cfe
Kim Alvefur <zash@zash.se>
2010-12-22 17:42:35 +0100

    util.pubsub: Return correct node in get_subscriptions()

--- util/pubsub.lua
+++ util/pubsub.lua
@@ -304,7 +304,7 @@ function service:get_subscriptions(node, actor, jid)
 			if node then -- Return only subscriptions to this node
 				if subscribed_nodes[node] then
 					ret[#ret+1] = {
-						node = node;
+						node = subscribed_node;
 						jid = jid;
 						subscription = node_obj.subscribers[jid];
 					};
