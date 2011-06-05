2eca719b5e68a5a4bdf29daa51896f08a3ffaacf
Matthew Wild <mwild1@gmail.com>
2011-06-05 23:55:16 +0100

    s2smanager: Fix letter case in log message (thanks darkrain)

--- core/s2smanager.lua
+++ core/s2smanager.lua
@@ -440,7 +440,7 @@ function streamopened(session, attr)
 		session.from_host = attr.from and nameprep(attr.from);
 	
 		session.streamid = uuid_gen();
-		(session.log or log)("debug", "incoming s2s received <stream:stream>");
+		(session.log or log)("debug", "Incoming s2s received <stream:stream>");
 		if session.to_host then
 			if not hosts[session.to_host] then
 				-- Attempting to connect to a host we don't serve
