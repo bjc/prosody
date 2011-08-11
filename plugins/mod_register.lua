309533f51398ae7cafd27becca54254998fb9acb
Florian Zeitz <florob@babelmonkeys.de>
2011-08-12 00:01:35 +0200

    mod_register: Add support for additional registration fields

--- plugins/mod_register.lua
+++ plugins/mod_register.lua
@@ -10,6 +10,7 @@
 local hosts = _G.hosts;
 local st = require "util.stanza";
 local datamanager = require "util.datamanager";
+local dataform_new = require "util.dataforms".new;
 local usermanager_user_exists = require "core.usermanager".user_exists;
 local usermanager_create_user = require "core.usermanager".create_user;
 local usermanager_set_password = require "core.usermanager".set_password;
@@ -20,6 +21,52 @@ local jid_bare = require "util.jid".bare;
 
 local compat = module:get_option_boolean("registration_compat", true);
 local allow_registration = module:get_option_boolean("allow_registration", false);
+local additional_fields = module:get_option("additional_registration_fields", {});
+
+local field_map = {
+	username = { name = "username", type = "text-single", label = "Username", required = true };
+	password = { name = "password", type = "text-private", label = "Password", required = true };
+	nick = { name = "nick", type = "text-single", label = "Nickname" };
+	name = { name = "name", type = "text-single", label = "Full Name" };
+	first = { name = "first", type = "text-single", label = "Given Name" };
+	last = { name = "last", type = "text-single", label = "Family Name" };
+	email = { name = "email", type = "text-single", label = "Email" };
+	address = { name = "address", type = "text-single", label = "Street" };
+	city = { name = "city", type = "text-single", label = "City" };
+	state = { name = "state", type = "text-single", label = "State" };
+	zip = { name = "zip", type = "text-single", label = "Postal code" };
+	phone = { name = "phone", type = "text-single", label = "Telephone number" };
+	url = { name = "url", type = "text-single", label = "Webpage" };
+	date = { name = "date", type = "text-single", label = "Birth date" };
+};
+
+local registration_form = dataform_new{
+	title = "Creating a new account";
+	instructions = "Choose a username and password for use with this service.";
+
+	field_map.username;
+	field_map.password;
+};
+
+local registration_query = st.stanza("query", {xmlns = "jabber:iq:register"})
+	:tag("instructions"):text("Choose a username and password for use with this service."):up()
+	:tag("username"):up()
+	:tag("password"):up();
+
+for _, field in ipairs(additional_fields) do
+	if type(field) == "table" then
+		registration_form[#registration_form + 1] = field;
+	else
+		if field:match("%+$") then
+			field = field:sub(1, #field - 1);
+			field_map[field].required = true;
+		end
+
+		registration_form[#registration_form + 1] = field_map[field];
+		registration_query:tag(field):up();
+	end
+end
+registration_query:add_child(registration_form:form());
 
 module:add_feature("jabber:iq:register");
 
@@ -67,6 +114,7 @@ local function handle_registration_stanza(event)
 			-- TODO datamanager should be able to delete all user data itself
 			datamanager.store(username, host, "vcard", nil);
 			datamanager.store(username, host, "private", nil);
+			datamanager.store(username, host, "account_details", nil);
 			datamanager.list_store(username, host, "offline", nil);
 			local bare = username.."@"..host;
 			for jid, item in pairs(roster) do
@@ -115,6 +163,29 @@ if compat then
 	end);
 end
 
+local function parse_response(query)
+	local form = query:get_child("x", "jabber:x:data");
+	if form then
+		return registration_form:data(form);
+	else
+		local data = {};
+		local errors = {};
+		for _, field in ipairs(registration_form) do
+			local name, required = field.name, field.required;
+			if field_map[name] then
+				data[name] = query:get_child_text(name);
+				if (not data[name] or #data[name] == 0) and required then
+					errors[name] = "Required value missing";
+				end
+			end
+		end
+		if next(errors) then
+			return data, errors;
+		end
+		return data;
+	end
+end
+
 local recent_ips = {};
 local min_seconds_between_registrations = module:get_option("min_seconds_between_registrations");
 local whitelist_only = module:get_option("whitelist_registration_only");
@@ -133,18 +204,16 @@ module:hook("stanza/iq/jabber:iq:register:query", function(event)
 		local query = stanza.tags[1];
 		if stanza.attr.type == "get" then
 			local reply = st.reply(stanza);
-			reply:tag("query", {xmlns = "jabber:iq:register"})
-				:tag("instructions"):text("Choose a username and password for use with this service."):up()
-				:tag("username"):up()
-				:tag("password"):up();
+			reply:add_child(registration_query);
 			session.send(reply);
 		elseif stanza.attr.type == "set" then
 			if query.tags[1] and query.tags[1].name == "remove" then
 				session.send(st.error_reply(stanza, "auth", "registration-required"));
 			else
-				local username = query:child_with_name("username");
-				local password = query:child_with_name("password");
-				if username and password then
+				local data, errors = parse_response(query);
+				if errors then
+					session.send(st.error_reply(stanza, "modify", "not-acceptable"));
+				else
 					-- Check that the user is not blacklisted or registering too often
 					if not session.ip then
 						module:log("debug", "User's IP not known; can't apply blacklist/whitelist");
@@ -166,28 +235,31 @@ module:hook("stanza/iq/jabber:iq:register:query", function(event)
 							ip.time = os_time();
 						end
 					end
-					-- FIXME shouldn't use table.concat
-					username = nodeprep(table.concat(username));
-					password = table.concat(password);
+					local username, password = nodeprep(data.username), data.password;
+					data.username, data.password = nil, nil;
 					local host = module.host;
 					if not username or username == "" then
 						session.send(st.error_reply(stanza, "modify", "not-acceptable", "The requested username is invalid."));
 					elseif usermanager_user_exists(username, host) then
 						session.send(st.error_reply(stanza, "cancel", "conflict", "The requested username already exists."));
 					else
+						-- TODO unable to write file, file may be locked, etc, what's the correct error?
+						local error_reply = st.error_reply(stanza, "wait", "internal-server-error", "Failed to write data to disk.");
 						if usermanager_create_user(username, password, host) then
+							if next(data) and not datamanager.store(username, host, "account_details", data) then
+								usermanager_delete_user(username, host);
+								session.send(error_reply);
+								return true;
+							end
 							session.send(st.reply(stanza)); -- user created!
 							module:log("info", "User account created: %s@%s", username, host);
 							module:fire_event("user-registered", {
 								username = username, host = host, source = "mod_register",
 								session = session });
 						else
-							-- TODO unable to write file, file may be locked, etc, what's the correct error?
-							session.send(st.error_reply(stanza, "wait", "internal-server-error", "Failed to write data to disk."));
+							session.send(error_reply);
 						end
 					end
-				else
-					session.send(st.error_reply(stanza, "modify", "not-acceptable"));
 				end
 			end
 		end
