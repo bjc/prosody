cfd622372fcdfb31f2dc400be443f7266032f444
Matthew Wild <mwild1@gmail.com>
2011-08-29 13:00:33 -0400

    util.throttle: Generic module by waqas to limit something over some time

---
+++ util/throttle.lua
@@ -0,0 +1,44 @@
+
+local gettime = require "socket".gettime;
+
+module "throttle"
+
+local throttle = {};
+local throttle_mt = { __index = throttle };
+
+function throttle:update()
+	local newt = gettime();
+	local elapsed = newt - self.t;
+	self.t = newt;
+	local balance = self.rate * elapsed + self.balance;
+	if balance > self.max then
+		self.balance = self.max;
+	else
+		self.balance = balance;
+	end
+	return self.balance;
+end
+
+function throttle:peek(cost)
+	cost = cost or 1;
+	return self.balance >= cost or self:update() >= cost;
+end
+
+function throttle:poll(cost, split)
+	if self:peek(cost) then
+		self.balance = self.balance - cost;
+		return true;
+	else
+		local balance = self.balance;
+		if split then
+			self.balance = 0;
+		end
+		return false, balance, (cost-self.balance);
+	end
+end
+
+function create(max, period)
+	return setmetatable({ rate = max / period, max = max, t = 0, balance = max }, throttle_mt);
+end
+
+return _M;
