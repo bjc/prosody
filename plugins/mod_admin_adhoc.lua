a4652f2478890ee6a86a28f5de5e3e8dcf10f9e7
Kim Alvefur <zash@zash.se>
2012-03-07 21:12:04 +0100

    mod_admin_adhoc: Import util.iterators properly

--- plugins/mod_admin_adhoc.lua
+++ plugins/mod_admin_adhoc.lua
@@ -10,7 +10,8 @@ local prosody = _G.prosody;
 local hosts = prosody.hosts;
 local t_concat = table.concat;
 
-require "util.iterators";
+local iterators = require "util.iterators";
+local keys, values = iterators.keys, iterators.values;
 local usermanager_user_exists = require "core.usermanager".user_exists;
 local usermanager_create_user = require "core.usermanager".create_user;
 local usermanager_get_password = require "core.usermanager".get_password;
