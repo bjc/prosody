60b60d9c30833600635e3a695362f903c812cd36
Matthew Wild <mwild1@gmail.com>
2012-04-21 22:53:22 +0100

    modulemanager: For children of shared modules, set module.environment to the empty environment for that module (useful to expose data or APIs at host.modules[module])

--- core/modulemanager.lua
+++ core/modulemanager.lua
@@ -144,6 +144,7 @@ local function do_load_module(host, module_name)
 			local ok, result, module_err = call_module_method(mod, "add_host", host_module_api);
 			if not ok or result == false then return nil, ok and module_err or result; end
 			local host_module = setmetatable({ module = host_module_api }, { __index = mod });
+			host_module.module.environment = host_module;
 			modulemap[host][module_name] = host_module;
 			return host_module;
 		end
