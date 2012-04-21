8af73c6e8ac4013229e599be6692447dbd53337d
Matthew Wild <mwild1@gmail.com>
2012-04-21 21:16:53 +0100

    mod_motd: Send only to resource coming online, not the user's bare JID (fixes #282)

--- plugins/mod_motd.lua
+++ plugins/mod_motd.lua
@@ -13,15 +13,15 @@ local motd_jid = module:get_option_string("motd_jid", host);
 
 if not motd_text then return; end
 
+local jid_join = require "util.jid".join;
 local st = require "util.stanza";
 
 motd_text = motd_text:gsub("^%s*(.-)%s*$", "%1"):gsub("\n%s+", "\n"); -- Strip indentation from the config
 
-module:hook("resource-bind",
-	function (event)
+module:hook("resource-bind", function (event)
 		local session = event.session;
 		local motd_stanza =
-			st.message({ to = session.username..'@'..session.host, from = motd_jid })
+			st.message({ to = jid_join(session.username, session.host, session.resource), from = motd_jid })
 				:tag("body"):text(motd_text);
 		core_route_stanza(hosts[host], motd_stanza);
 		module:log("debug", "MOTD send to user %s@%s", session.username, session.host);
