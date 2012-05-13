d61c6b1760b28051f551acd69a1f18962133bcb0
Matthew Wild <mwild1@gmail.com>
2012-05-13 21:07:00 +0100

    muc.lib: Fix for traceback when no history length is set for the room

--- plugins/muc/muc.lib.lua
+++ plugins/muc/muc.lib.lua
@@ -135,7 +135,7 @@ function room_mt:broadcast_message(stanza, historic)
 		stanza:tag("x", {xmlns = "jabber:x:delay", from = muc_domain, stamp = datetime.legacy()}):up(); -- XEP-0091 (deprecated)
 		local entry = { stanza = stanza, stamp = stamp };
 		t_insert(history, entry);
-		while #history > self._data.history_length do t_remove(history, 1) end
+		while #history > (self._data.history_length or default_history_length) do t_remove(history, 1) end
 	end
 end
 function room_mt:broadcast_except_nick(stanza, nick)
