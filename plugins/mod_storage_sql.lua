07bb8a42ce660684bbe4742aeefd5ea92092a37e
Kim Alvefur <zash@zash.se>
2012-09-17 05:42:10 +0200

    storagemanager, datamanager, mod_storage_{internal,sql}: Replace list_stores() with an iterator version

--- plugins/mod_storage_sql.lua
+++ plugins/mod_storage_sql.lua
@@ -374,10 +374,9 @@ function driver:open(store, typ)
 	return nil, "unsupported-store";
 end
 
-function driver:list_stores(username) -- Not to be confused with the list store type
-	local sql = (username == true
-		and "SELECT DISTINCT `store` FROM `prosody` WHERE `host`=? AND `user`!=?"
-		or  "SELECT DISTINCT `store` FROM `prosody` WHERE `host`=? AND `user`=?");
+function driver:stores(username) -- Not to be confused with the list store type
+	local sql = "SELECT DISTINCT `store` FROM `prosody` WHERE `host`=? AND `user`" ..
+		(username == true and "!=?" or "=?");
 	if username == true or not username then
 		username = "";
 	end
@@ -385,11 +384,11 @@ function driver:list_stores(username) -- Not to be confused with the list store 
 	if not stmt then
 		return rollback(nil, err);
 	end
-	local stores = {};
-	for row in stmt:rows() do
-		stores[#stores+1] = row[1];
-	end
-	return commit(stores);
+	local next = stmt:rows();
+	return commit(function()
+		local row = next();
+		return row and row[1];
+	end);
 end
 
 function driver:purge(username)
