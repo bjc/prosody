9c4985d9d82aaf504ecda0026138608a119b0d78
Kim Alvefur <zash@zash.se>
2013-03-23 23:30:13 +0100

    mod_s2s: Keep the dns answer object around a while so plugins can look at it

--- plugins/mod_s2s/s2sout.lib.lua
+++ plugins/mod_s2s/s2sout.lib.lua
@@ -90,7 +90,7 @@ function s2sout.attempt_connection(host_session, err)
 			host_session.connecting = nil;
 			if answer and #answer > 0 then
 				log("debug", "%s has SRV records, handling...", to_host);
-				local srv_hosts = {};
+				local srv_hosts = { answer = answer };
 				host_session.srv_hosts = srv_hosts;
 				for _, record in ipairs(answer) do
 					t_insert(srv_hosts, record.srv);
