2a89893ebe9a9adc260803fdb4fc362670565b2a
Matthew Wild <mwild1@gmail.com>
2013-04-11 17:39:10 +0100

    net.http.parser: Break when no more usable data in buffer (client part of e5ec60dfb202)

--- net/http/parser.lua
+++ net/http/parser.lua
@@ -136,6 +136,8 @@ function httpstream.new(success_cb, error_cb, parser_type, options_cb)
 						elseif len and #buf >= len then
 							packet.body, buf = buf:sub(1, len), buf:sub(len + 1);
 							state = nil; success_cb(packet);
+						else
+							break;
 						end
 					elseif #buf >= len then
 						packet.body, buf = buf:sub(1, len), buf:sub(len + 1);
