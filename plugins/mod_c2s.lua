b6ecf01333bcb773beb784e2be93af305dd1cf01
Kim Alvefur <zash@zash.se>
2013-06-11 21:18:51 +0200

    mod_c2s: Become a shared module and allow being disabled on some virtualhosts

--- plugins/mod_c2s.lua
+++ plugins/mod_c2s.lua
@@ -50,7 +50,7 @@ function stream_callbacks.streamopened(session, attr)
 	session.streamid = uuid_generate();
 	(session.log or session)("debug", "Client sent opening <stream:stream> to %s", session.host);
 
-	if not hosts[session.host] then
+	if not hosts[session.host] or not hosts[session.host].modules.c2s then
 		-- We don't serve this host...
 		session:close{ condition = "host-unknown", text = "This server does not serve "..tostring(session.host)};
 		return;
@@ -272,6 +272,8 @@ end
 function listener.associate_session(conn, session)
 	sessions[conn] = session;
 end
+
+function module.add_host() end
 
 module:hook("server-stopping", function(event)
 	local reason = event.reason;
