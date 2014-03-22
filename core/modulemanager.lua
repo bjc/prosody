7beffae4db37cdb297f9fc20d549f0b0c7458f56
Kim Alvefur <zash@zash.se>
2014-03-22 12:42:01 +0100

    modulemanager: Load mod_saslauth on components by default

--- core/modulemanager.lua
+++ core/modulemanager.lua
@@ -30,7 +30,7 @@ pcall = function(f, ...)
 end
 
 local autoload_modules = {"presence", "message", "iq", "offline", "c2s", "s2s"};
-local component_inheritable_modules = {"tls", "dialback", "iq", "s2s"};
+local component_inheritable_modules = {"tls", "saslauth", "dialback", "iq", "s2s"};
 
 -- We need this to let modules access the real global namespace
 local _G = _G;
