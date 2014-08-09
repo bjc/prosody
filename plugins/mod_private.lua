503aba403d99eeedf2cc8ebb6c7df50e983f538d
Kim Alvefur <zash@zash.se>
2014-08-09 21:48:11 +0200

    mod_private: Cleanup and more tailcalls

--- plugins/mod_private.lua
+++ plugins/mod_private.lua
@@ -15,38 +15,34 @@ module:add_feature("jabber:iq:private");
 
 module:hook("iq/self/jabber:iq:private:query", function(event)
 	local origin, stanza = event.origin, event.stanza;
-	local type = stanza.attr.type;
 	local query = stanza.tags[1];
-	if #query.tags == 1 then
-		local tag = query.tags[1];
-		local key = tag.name..":"..tag.attr.xmlns;
-		local data, err = private_storage:get(origin.username);
-		if err then
-			origin.send(st.error_reply(stanza, "wait", "internal-server-error"));
-			return true;
-		end
-		if stanza.attr.type == "get" then
-			if data and data[key] then
-				origin.send(st.reply(stanza):tag("query", {xmlns = "jabber:iq:private"}):add_child(st.deserialize(data[key])));
-			else
-				origin.send(st.reply(stanza):add_child(stanza.tags[1]));
-			end
-		else -- set
-			if not data then data = {}; end;
-			if #tag == 0 then
-				data[key] = nil;
-			else
-				data[key] = st.preserialize(tag);
-			end
-			-- TODO delete datastore if empty
-			if private_storage:set(origin.username, data) then
-				origin.send(st.reply(stanza));
-			else
-				origin.send(st.error_reply(stanza, "wait", "internal-server-error"));
-			end
-		end
-	else
-		origin.send(st.error_reply(stanza, "modify", "bad-format"));
+	if #query.tags ~= 1 then
+		return origin.send(st.error_reply(stanza, "modify", "bad-format"));
 	end
-	return true;
+	local tag = query.tags[1];
+	local key = tag.name..":"..tag.attr.xmlns;
+	local data, err = private_storage:get(origin.username);
+	if err then
+		return origin.send(st.error_reply(stanza, "wait", "internal-server-error", err));
+	end
+	if stanza.attr.type == "get" then
+		if data and data[key] then
+			return origin.send(st.reply(stanza):query("jabber:iq:private"):add_child(st.deserialize(data)));
+		else
+			return origin.send(st.reply(stanza):add_child(query));
+		end
+	else -- type == set
+		if not data then data = {}; end;
+		if #tag == 0 then
+			data[key] = nil;
+		else
+			data[key] = st.preserialize(tag);
+		end
+		-- TODO delete datastore if empty
+		local ok, err = private_storage:set(origin.username, data);
+		if not ok then
+			return origin.send(st.error_reply(stanza, "wait", "internal-server-error", err));
+		end
+		return origin.send(st.reply(stanza));
+	end
 end);
