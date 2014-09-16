1ebc03c62252c2fa1e2c431465ed3e9b46bf3c51
Waqas Hussain <waqas20@gmail.com>
2014-09-16 12:27:05 -0400

    plugins/mod_muc_unique: Fix undefined global access (thanks Lance)

--- plugins/mod_muc_unique.lua
+++ plugins/mod_muc_unique.lua
@@ -1,7 +1,8 @@
 -- XEP-0307: Unique Room Names for Multi-User Chat
+local st = require "util.stanza";
 local uuid_gen = require "util.uuid".generate;
 module:add_feature "http://jabber.org/protocol/muc#unique"
-module:hook("iq-get/host/http://jabber.org/protocol/muc#unique:unique", function()
+module:hook("iq-get/host/http://jabber.org/protocol/muc#unique:unique", function(event)
 	local origin, stanza = event.origin, event.stanza;
 	origin.send(st.reply(stanza)
 		:tag("unique", {xmlns = "http://jabber.org/protocol/muc#unique"})
