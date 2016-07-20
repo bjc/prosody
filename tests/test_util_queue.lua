fbd4721af01a2a50629ed089739d42adfd7a9dd5
Kim Alvefur <zash@zash.se>
2016-07-20 17:24:49 +0200

    tests: Adapt test for util.queue to the test framework

--- tests/test_util_queue.lua
+++ tests/test_util_queue.lua
@@ -1,68 +1,72 @@
-local new = require "util.queue".new;
 
-local q = new(10);
+function new(new)
+	do
+		local q = new(10);
 
-assert(q.size == 10);
-assert(q:count() == 0);
+		assert_equal(q.size, 10);
+		assert_equal(q:count(), 0);
 
-assert(q:push("one"));
-assert(q:push("two"));
-assert(q:push("three"));
+		assert_is(q:push("one"));
+		assert_is(q:push("two"));
+		assert_is(q:push("three"));
 
-for i = 4, 10 do
-	print("pushing "..i)
-	assert(q:push("hello"));
-	assert(q:count() == i, "count is not "..i.."("..q:count()..")");
+		for i = 4, 10 do
+			assert_is(q:push("hello"));
+			assert_equal(q:count(), i, "count is not "..i.."("..q:count()..")");
+		end
+		assert_equal(q:push("hello"), nil, "queue overfull!");
+		assert_equal(q:push("hello"), nil, "queue overfull!");
+		assert_equal(q:pop(), "one", "queue item incorrect");
+		assert_equal(q:pop(), "two", "queue item incorrect");
+		assert_is(q:push("hello"));
+		assert_is(q:push("hello"));
+		assert_equal(q:pop(), "three", "queue item incorrect");
+		assert_is(q:push("hello"));
+		assert_equal(q:push("hello"), nil, "queue overfull!");
+		assert_equal(q:push("hello"), nil, "queue overfull!");
+
+		assert_equal(q:count(), 10, "queue count incorrect");
+
+		for _ = 1, 10 do
+			assert_equal(q:pop(), "hello", "queue item incorrect");
+		end
+
+		assert_equal(q:count(), 0, "queue count incorrect");
+
+		assert_is(q:push(1));
+		for i = 1, 1001 do
+			assert_equal(q:pop(), i);
+			assert_equal(q:count(), 0);
+			assert_is(q:push(i+1));
+			assert_equal(q:count(), 1);
+		end
+		assert_equal(q:pop(), 1002);
+		assert_is(q:push(1));
+		for i = 1, 1000000 do
+			q:pop();
+			q:push(i+1);
+		end
+	end
+
+	do
+		-- Test queues that purge old items when pushing to a full queue
+		local q = new(10, true);
+
+		for i = 1, 10 do
+			q:push(i);
+		end
+
+		assert_equal(q:count(), 10);
+
+		assert_is(q:push(11));
+		assert_equal(q:count(), 10);
+		assert_equal(q:pop(), 2); -- First item should have been purged
+
+		for i = 12, 32 do
+			assert_is(q:push(i));
+		end
+
+		assert_equal(q:count(), 10);
+		assert_equal(q:pop(), 23);
+	end
 end
-assert(q:push("hello") == nil, "queue overfull!");
-assert(q:push("hello") == nil, "queue overfull!");
-assert(q:pop() == "one", "queue item incorrect");
-assert(q:pop() == "two", "queue item incorrect");
-assert(q:push("hello"));
-assert(q:push("hello"));
-assert(q:pop() == "three", "queue item incorrect");
-assert(q:push("hello"));
-assert(q:push("hello") == nil, "queue overfull!");
-assert(q:push("hello") == nil, "queue overfull!");
-
-assert(q:count() == 10, "queue count incorrect");
-
-for i = 1, 10 do
-	assert(q:pop() == "hello", "queue item incorrect");
-end
-
-assert(q:count() == 0, "queue count incorrect");
-
-assert(q:push(1));
-for i = 1, 1001 do
-	assert(q:pop() == i);
-	assert(q:count() == 0);
-	assert(q:push(i+1));
-	assert(q:count() == 1);
-end
-assert(q:pop() == 1002);
-assert(q:push(1));
-for i = 1, 1000000 do
-	q:pop();
-	q:push(i+1);
-end
-
--- Test queues that purge old items when pushing to a full queue
-local q = new(10, true);
-
-for i = 1, 10 do
-	q:push(i);
-end
-
-assert(q:count() == 10);
-
-assert(q:push(11));
-assert(q:count() == 10);
-assert(q:pop() == 2); -- First item should have been purged
-
-for i = 12, 32 do
-	assert(q:push(i));
-end
-
-assert(q:count() == 10);
-assert(q:pop() == 23);
