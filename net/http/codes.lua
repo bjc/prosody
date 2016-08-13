8165818e268f3995d34bdd5f4fcf61ff196446ae
Kim Alvefur <zash@zash.se>
2016-08-13 16:11:30 +0200

    net.http.codes: Add HTTP status code 451 Unavailable For Legal Reasons from RFC 7725

--- net/http/codes.lua
+++ net/http/codes.lua
@@ -55,6 +55,7 @@ local response_codes = {
 	[428] = "Precondition Required";
 	[429] = "Too Many Requests";
 	[431] = "Request Header Fields Too Large";
+	[451] = "Unavailable For Legal Reasons";
 
 	[500] = "Internal Server Error";
 	[501] = "Not Implemented";
