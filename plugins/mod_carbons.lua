c89d3f3176b3cb71b4945c04c42d34f2c09d5cb5
Kim Alvefur <zash@zash.se>
2016-09-23 14:06:25 +0200

    mod_carbons: Compare with correct variable

--- plugins/mod_carbons.lua
+++ plugins/mod_carbons.lua
@@ -67,7 +67,7 @@ local function message_handler(event, c2s)
 	elseif stanza:get_child("no-copy", "urn:xmpp:hints") then
 		module:log("debug", "Message has no-copy hint, ignoring");
 		return
-	elseif not c2s and orig_from ~= orig_from and stanza:get_child("x", "http://jabber.org/protocol/muc#user") then
+	elseif not c2s and bare_jid ~= orig_from and stanza:get_child("x", "http://jabber.org/protocol/muc#user") then
 		module:log("debug", "MUC PM, ignoring");
 		return
 	end
