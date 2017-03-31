d2b589c2aa0df1e0c372f4f94049e20ef478c609
Kim Alvefur <zash@zash.se>
2017-03-31 17:50:19 +0200

    mod_offline: Switch to using archive store via the storagemanager API

--- plugins/mod_offline.lua
+++ plugins/mod_offline.lua
@@ -7,29 +7,24 @@
 --
 
 
-local datamanager = require "util.datamanager";
-local st = require "util.stanza";
 local datetime = require "util.datetime";
-local ipairs = ipairs;
 local jid_split = require "util.jid".split;
+
+local offline_messages = module:open_store("offline", "archive");
 
 module:add_feature("msgoffline");
 
 module:hook("message/offline/handle", function(event)
 	local origin, stanza = event.origin, event.stanza;
 	local to = stanza.attr.to;
-	local node, host;
+	local node;
 	if to then
-		node, host = jid_split(to)
+		node = jid_split(to)
 	else
-		node, host = origin.username, origin.host;
+		node = origin.username;
 	end
 
-	stanza.attr.stamp, stanza.attr.stamp_legacy = datetime.datetime(), datetime.legacy();
-	local result = datamanager.list_append(node, host, "offline", st.preserialize(stanza));
-	stanza.attr.stamp, stanza.attr.stamp_legacy = nil, nil;
-
-	return result;
+	return offline_messages:append(node, nil, stanza);
 end, -1);
 
 module:hook("message/offline/broadcast", function(event)
@@ -37,15 +32,12 @@ module:hook("message/offline/broadcast", function(event)
 
 	local node, host = origin.username, origin.host;
 
-	local data = datamanager.list_load(node, host, "offline");
+	local data = offline_messages:find(node);
 	if not data then return true; end
-	for _, stanza in ipairs(data) do
-		stanza = st.deserialize(stanza);
-		stanza:tag("delay", {xmlns = "urn:xmpp:delay", from = host, stamp = stanza.attr.stamp}):up(); -- XEP-0203
-		stanza:tag("x", {xmlns = "jabber:x:delay", from = host, stamp = stanza.attr.stamp_legacy}):up(); -- XEP-0091 (deprecated)
-		stanza.attr.stamp, stanza.attr.stamp_legacy = nil, nil;
+	for _, stanza, when in data do
+		stanza:tag("delay", {xmlns = "urn:xmpp:delay", from = host, stamp = datetime.datetime(when)}):up(); -- XEP-0203
 		origin.send(stanza);
 	end
-	datamanager.list_store(node, host, "offline", nil);
+	offline_messages:delete(node);
 	return true;
 end, -1);
