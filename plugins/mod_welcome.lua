463ae920091de0ba7be52f6526620cd6eaac8f05
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2017-05-30 20:52:22 +0100

    mod_welcome: Return the pointer to the root of the stanza, fixes a bug similar to #922.

--- plugins/mod_welcome.lua
+++ plugins/mod_welcome.lua
@@ -14,8 +14,8 @@ local st = require "util.stanza";
 module:hook("user-registered",
 	function (user)
 		local welcome_stanza =
-			st.message({ to = user.username.."@"..user.host, from = host })
-				:tag("body"):text(welcome_text:gsub("$(%w+)", user));
+			st.message({ to = user.username.."@"..user.host, from = host },
+				welcome_text:gsub("$(%w+)", user));
 		module:send(welcome_stanza);
 		module:log("debug", "Welcomed user %s@%s", user.username, user.host);
 	end);
