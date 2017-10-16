f2bdafd709ba284b5d9d74bbf8924829b14a8060
Kim Alvefur <zash@zash.se>
2017-10-16 22:12:14 +0200

    util.pubsub: Remove inclusion of publisher
    util.pubsub should stay agnostic of what data types are published and
    this depended on util.stanza

--- util/pubsub.lua
+++ util/pubsub.lua
@@ -271,9 +271,6 @@ function service:publish(node, actor, id, item)
 		node_obj = self.nodes[node];
 	end
 	local node_data = self.data[node];
-	if type(actor) == "string" then
-		item.attr.publisher = actor;
-	end
 	local ok = node_data:set(id, item);
 	if not ok then
 		return nil, "internal-server-error";
