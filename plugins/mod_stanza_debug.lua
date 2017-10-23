695e80fad2270d317dd3b45b0d2ef2c9aec7062d
Kim Alvefur <zash@zash.se>
2017-10-23 15:23:03 +0200

    mod_stanza_debug: Logs full stanzas sent and received for debugging purposes

---
+++ plugins/mod_stanza_debug.lua
@@ -0,0 +1,29 @@
+module:set_global();
+
+local tostring = tostring;
+local filters = require "util.filters";
+
+local function log_send(t, session)
+	if t and t ~= "" and t ~= " " then
+		session.log("debug", "SEND: %s", tostring(t));
+	end
+	return t;
+end
+
+local function log_recv(t, session)
+	if t and t ~= "" and t ~= " " then
+		session.log("debug", "RECV: %s", tostring(t));
+	end
+	return t;
+end
+
+local function init_raw_logging(session)
+	filters.add_filter(session, "stanzas/in",  log_recv, -10000);
+	filters.add_filter(session, "stanzas/out", log_send,  10000);
+end
+
+filters.add_filter_hook(init_raw_logging);
+
+function module.unload()
+	filters.remove_filter_hook(init_raw_logging);
+end
