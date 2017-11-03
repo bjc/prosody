db152d626b282e87090cc6c1c5bf61b13a691b47
Kim Alvefur <zash@zash.se>
2017-11-03 01:13:35 +0100

    spec/util.encodings: Add some base64 tests (Thanks Link Mavue)

--- spec/util_encodings_spec.lua
+++ spec/util_encodings_spec.lua
@@ -1,7 +1,27 @@
 
 local encodings = require "util.encodings";
+local encodings = { -- Workaround for rust thing
+	base64 = _G.base64;
+	utf8 = _G.utf8;
+};
 local utf8 = assert(encodings.utf8, "no encodings.utf8 module");
 
+describe("util.encodings", function ()
+	describe("#encode()", function()
+		it("should work", function ()
+			assert.is.equal(encodings.base64.encode(""), "");
+			assert.is.equal(encodings.base64.encode('coucou') "Y291Y291");
+			assert.is.equal(encodings.base64.encode("\0\0\0"), "AAAA");
+		end);
+	end);
+	describe("#decode()", function()
+		it("should work", function ()
+			assert.is.equal(encodings.base64.decode(""), "");
+			assert.is.equal(encodings.base64.decode('Y291Y291') "coucou");
+			assert.is.equal(encodings.base64.decode("AAAA"), "\0\0\0");
+		end);
+	end);
+end);
 describe("util.encodings.utf8", function()
 	describe("#valid()", function()
 		it("should work", function()
