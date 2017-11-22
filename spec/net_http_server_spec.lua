9290f9588a329280e41dd51fbb2895e46e7e29ed
Kim Alvefur <zash@zash.se>
2017-11-22 02:40:42 +0100

    spec/net.http.server: Add test for #1044

---
+++ spec/net_http_server_spec.lua
@@ -0,0 +1,12 @@
+describe("net.http.server", function ()
+	local server = require "net.http.server";
+	describe("events", function ()
+		it("should work with util.helpers", function ()
+			-- See #1044
+			server.add_handler("GET host/foo/*", function () end, 0);
+			server.add_handler("GET host/foo/bar", function () end, 0);
+			local helpers = require "util.helpers";
+			assert.is.string(helpers.show_events(server._events));
+		end);
+	end);
+end);
