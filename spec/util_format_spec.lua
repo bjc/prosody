f29665ce04ca8006c5c7124d327ba59171a30513
Kim Alvefur <zash@zash.se>
2018-03-17 19:43:20 +0100

    util.format: Fix tests to have expected value first

--- spec/util_format_spec.lua
+++ spec/util_format_spec.lua
@@ -3,11 +3,11 @@ local format = require "util.format".format;
 describe("util.format", function()
 	describe("#format()", function()
 		it("should work", function()
-			assert.equal(format("%s", "hello"), "hello");
-			assert.equal(format("%s"), "<nil>");
-			assert.equal(format("%s", true), "true");
-			assert.equal(format("%d", true), "[true]");
-			assert.equal(format("%%", true), "% [true]");
+			assert.equal("hello", format("%s", "hello"));
+			assert.equal("<nil>", format("%s"));
+			assert.equal("true", format("%s", true));
+			assert.equal("[true]", format("%d", true));
+			assert.equal("% [true]", format("%%", true));
 		end);
 	end);
 end);
