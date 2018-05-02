31c92dfc4b0811cbdbdb2dc6b7ada31b14406488
Kim Alvefur <zash@zash.se>
2018-05-02 12:39:42 +0200

    mod_privacy: Silence long line warning [luacheck]

--- plugins/mod_privacy.lua
+++ plugins/mod_privacy.lua
@@ -7,7 +7,7 @@
 -- COPYING file in the source package for more information.
 --
 
-
+-- luacheck: ignore 631
 -- COMPAT w/ pre 0.10
 module:log("error", "The mod_privacy plugin has been replaced by mod_blocklist. Please update your config. For more information see https://prosody.im/doc/modules/mod_privacy");
 module:depends("blocklist");
