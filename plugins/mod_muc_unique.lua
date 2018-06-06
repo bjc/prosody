037968b57bd77c3c0f58a28792dfc0640c15a071
Kim Alvefur <zash@zash.se>
2018-06-06 13:33:37 +0200

    mod_muc_unique: Use util.id for more compact identifiers

--- plugins/mod_muc_unique.lua
+++ plugins/mod_muc_unique.lua
@@ -1,12 +1,12 @@
 -- XEP-0307: Unique Room Names for Multi-User Chat
 local st = require "util.stanza";
-local uuid_gen = require "util.uuid".generate;
+local unique_name = require "util.id".medium;
 module:add_feature "http://jabber.org/protocol/muc#unique"
 module:hook("iq-get/host/http://jabber.org/protocol/muc#unique:unique", function(event)
 	local origin, stanza = event.origin, event.stanza;
 	origin.send(st.reply(stanza)
 		:tag("unique", {xmlns = "http://jabber.org/protocol/muc#unique"})
-		:text(uuid_gen()) -- FIXME Random UUIDs can theoretically have collisions
+		:text(unique_name():lower())
 	);
 	return true;
 end,-1);
