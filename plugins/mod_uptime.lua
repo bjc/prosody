b7b67000ca6533c88a40a7ce1ded6a476c7a4538
Emmanuel Gil Peyrot <linkmauve@linkmauve.fr>
2018-08-24 20:35:17 +0200

    mod_uptime: Simplify iq handling by hooking on iq-get/ instead of iq/.

--- plugins/mod_uptime.lua
+++ plugins/mod_uptime.lua
@@ -14,12 +14,10 @@ module:hook_global("server-started", function() start_time = prosody.start_time 
 -- XEP-0012: Last activity
 module:add_feature("jabber:iq:last");
 
-module:hook("iq/host/jabber:iq:last:query", function(event)
+module:hook("iq-get/host/jabber:iq:last:query", function(event)
 	local origin, stanza = event.origin, event.stanza;
-	if stanza.attr.type == "get" then
-		origin.send(st.reply(stanza):tag("query", {xmlns = "jabber:iq:last", seconds = tostring(os.difftime(os.time(), start_time))}));
-		return true;
-	end
+	origin.send(st.reply(stanza):tag("query", {xmlns = "jabber:iq:last", seconds = tostring(os.difftime(os.time(), start_time))}));
+	return true;
 end);
 
 -- Ad-hoc command
