f364ac898879be9a44fe5d1c3566c89407cbd87a
Kim Alvefur <zash@zash.se>
2018-10-21 14:33:26 +0200

    mod_compression: Remove tombstone (deprecated in 0.10)

--- plugins/mod_compression.lua
+++
@@ -1,9 +0,0 @@
--- Prosody IM
--- Copyright (C) 2016 Matthew Wild
---
--- This project is MIT/X11 licensed. Please see the
--- COPYING file in the source package for more information.
---
-
--- COMPAT w/ pre-0.10 configs
-error("mod_compression has been removed in Prosody 0.10+. Please see https://prosody.im/doc/modules/mod_compression for more information.");
