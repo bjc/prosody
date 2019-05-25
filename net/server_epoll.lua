d06300c456ad2209a500e64a6efac1094d58bdea
Kim Alvefur <zash@zash.se>
2019-05-25 16:09:26 +0200

    net.server_epoll: Return listener error message

--- net/server_epoll.lua
+++ net/server_epoll.lua
@@ -162,7 +162,7 @@ function interface:on(what, ...)
 	local ok, err = pcall(listener, self, ...);
 	if not ok then
 		log("error", "Error calling on%s: %s", what, err);
-		return;
+		return nil, err;
 	end
 	return err;
 end
