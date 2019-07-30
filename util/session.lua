73fab2dc0d581b9744b611d97650cbe09ac6aaca
Kim Alvefur <zash@zash.se>
2019-07-30 02:24:06 +0200

    util.session: Remove tostring call from logging
    Taken care of by loggingmanager now

--- util/session.lua
+++ util/session.lua
@@ -31,7 +31,7 @@ local function set_send(session)
 	local conn = session.conn;
 	if not conn then
 		function session.send(data)
-			session.log("debug", "Discarding data sent to unconnected session: %s", tostring(data));
+			session.log("debug", "Discarding data sent to unconnected session: %s", data);
 			return false;
 		end
 		return session;
@@ -47,7 +47,7 @@ local function set_send(session)
 			if t then
 				local ret, err = w(conn, t);
 				if not ret then
-					session.log("debug", "Error writing to connection: %s", tostring(err));
+					session.log("debug", "Error writing to connection: %s", err);
 					return false, err;
 				end
 			end
