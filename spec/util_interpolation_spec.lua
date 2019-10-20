8b19b4435f6feded2bfffa4ee5a11716579ec7ea
Kim Alvefur <zash@zash.se>
2019-10-20 20:53:41 +0200

    util.interpolation: Test #1452

--- spec/util_interpolation_spec.lua
+++ spec/util_interpolation_spec.lua
@@ -1,5 +1,5 @@
 local template = [[
-{greet!}, {name?world}!
+{greet!?Hi}, {name?world}!
 ]];
 local expect1 = [[
 Hello, WORLD!
@@ -7,11 +7,15 @@ Hello, WORLD!
 local expect2 = [[
 Hello, world!
 ]];
+local expect3 = [[
+Hi, YOU!
+]];
 
 describe("util.interpolation", function ()
 	it("renders", function ()
 		local render = require "util.interpolation".new("%b{}", string.upper);
 		assert.equal(expect1, render(template, { greet = "Hello", name = "world" }));
 		assert.equal(expect2, render(template, { greet = "Hello" }));
+		assert.equal(expect3, render(template, { name = "you" }));
 	end);
 end);
