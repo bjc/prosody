37a97e23ac9636177c989f90c97d2708da21ae0a
Kim Alvefur <zash@zash.se>
2019-10-20 20:58:19 +0200

    util.interpolation: Test map syntax

--- spec/util_interpolation_spec.lua
+++ spec/util_interpolation_spec.lua
@@ -17,6 +17,12 @@ local expect_array = [[
 1. HELLO
 2. WORLD
 ]]
+local template_map = [[
+{foo%{idx}: {item!}
+}]]
+local expect_map = [[
+FOO: bar
+]]
 
 describe("util.interpolation", function ()
 	it("renders", function ()
@@ -25,5 +31,6 @@ describe("util.interpolation", function ()
 		assert.equal(expect2, render(template, { greet = "Hello" }));
 		assert.equal(expect3, render(template, { name = "you" }));
 		assert.equal(expect_array, render(template_array, { foo = { "Hello", "World" } }));
+		assert.equal(expect_map, render(template_map, { foo = { foo = "bar" } }));
 	end);
 end);
