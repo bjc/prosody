8703eaa89a6cf42d6fe4d8e1083bbb029afcf7e9
Kim Alvefur <zash@zash.se>
2019-11-23 17:15:34 +0100

    mod_csi: Cache CSI module availability to improve readabilty

--- plugins/mod_csi.lua
+++ plugins/mod_csi.lua
@@ -2,8 +2,9 @@ local st = require "util.stanza";
 local xmlns_csi = "urn:xmpp:csi:0";
 local csi_feature = st.stanza("csi", { xmlns = xmlns_csi });
 
+local csi_handler_available = nil;
 module:hook("stream-features", function (event)
-	if event.origin.username and prosody.hosts[module.host].events._handlers["csi-client-active"] then
+	if event.origin.username and csi_handler_available then
 		event.features:add_child(csi_feature);
 	end
 end);
@@ -23,8 +24,10 @@ module:hook("stanza/"..xmlns_csi..":inactive", refire_event("csi-client-inactive
 
 function module.load()
 	if prosody.hosts[module.host].events._handlers["csi-client-active"] then
+		csi_handler_available = true;
 		module:set_status("core", "CSI handler module loaded");
 	else
+		csi_handler_available = false;
 		module:set_status("warn", "No CSI handler module loaded");
 	end
 end
