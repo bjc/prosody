ee3a103480d2af2dabf83174cd64e6917df5972d
Kim Alvefur <zash@zash.se>
2019-12-14 22:47:41 +0100

    util.stanza: Accept util.error object to error_reply
    If we're moving towards util.error as the standard error container then
    this makes sense.
    
    This may allow for future extensibility without needing a lot of
    optional arguments.

--- util/stanza.lua
+++ util/stanza.lua
@@ -458,6 +458,9 @@ local function error_reply(orig, error_type, condition, error_message, error_by)
 	if t.attr.from == error_by then
 		error_by = nil;
 	end
+	if type(error_type) == "table" then -- an util.error or similar object
+		error_type, condition, error_message = error_type.type, error_type.condition, error_type.text;
+	end
 	t:tag("error", {type = error_type, by = error_by}) --COMPAT: Some day xmlns:stanzas goes here
 	:tag(condition, xmpp_stanzas_attr):up();
 	if error_message then t:text_tag("text", error_message, xmpp_stanzas_attr); end
