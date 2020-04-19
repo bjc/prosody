e6a9933484cf49c89ef25200cb8a934ea4362310
JC Brand <jc@opkode.com>
2020-04-19 21:49:45 +0200

    MUC: Add support for presence probes (fixes #1535)
    The following patch allows Prosody to respond to `probe` presences and send out the probed occupant's current presence.
    
    This is based on line 17.3 in XEP-0045:
    
        A MUC service MAY handle presence probes sent to the room JID <room@service> or an occupant JID <room@service/nick>
        (e.g, these might be sent by an occupant's home server to determine if the room is still online or to synchronize
        presence information if the user or the user's server has gone offline temporarily or has started sharing presence again,
        as for instance when Stanza Interception and Filtering Technology (XEP-0273) is used).

--- plugins/muc/hats.lib.lua
+++ plugins/muc/hats.lib.lua
@@ -6,8 +6,10 @@ local xmlns_hats = "xmpp:prosody.im/protocol/hats:1";
 -- Strip any hats claimed by the client (to prevent spoofing)
 muc_util.add_filtered_namespace(xmlns_hats);
 
+
 module:hook("muc-build-occupant-presence", function (event)
-	local aff_data = event.room:get_affiliation_data(event.occupant.bare_jid);
+	local bare_jid = event.occupant and event.occupant.bare_jid or event.bare_jid;
+	local aff_data = event.room:get_affiliation_data(bare_jid);
 	local hats = aff_data and aff_data.hats;
 	if not hats then return; end
 	local hats_el;
