6674d10a2294a7f767fcdd0fbffe01060d522b32
Kim Alvefur <zash@zash.se>
2020-09-26 18:12:18 +0200

    util.stanza: Extract Application-Specific Condition from errors
    API change

--- spec/util_stanza_spec.lua
+++ spec/util_stanza_spec.lua
@@ -276,6 +276,19 @@ describe("util.stanza", function()
 		end);
 	end);
 
+	describe("#get_error()", function ()
+		describe("basics", function ()
+			local s = st.message();
+			local e = st.error_reply(s, "cancel", "not-acceptable", "UNACCEPTABLE!!!! ONE MILLION YEARS DUNGEON!")
+				:tag("dungeon", { xmlns = "urn:uuid:c9026187-5b05-4e70-b265-c3b6338a7d0f", period="1000000years"});
+			local typ, cond, text, extra = e:get_error();
+			assert.equal("cancel", typ);
+			assert.equal("not-acceptable", cond);
+			assert.equal("UNACCEPTABLE!!!! ONE MILLION YEARS DUNGEON!", text);
+			assert.not_nil(extra)
+		end)
+	end)
+
 	describe("should reject #invalid", function ()
 		local invalid_names = {
 			["empty string"] = "", ["characters"] = "<>";
