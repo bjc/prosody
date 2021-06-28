27943c671cab85e37cac86e96f9bd0a064c1c689
Kim Alvefur <zash@zash.se>
2021-06-28 03:56:45 +0200

    util.bit53: Add left- and right shift operations
    While not used by anything in Prosody, it is known to be used by some
    3rd party modules.

--- util/bit53.lua
+++ util/bit53.lua
@@ -3,5 +3,7 @@ return {
 	band   = function (a, b) return a & b end;
 	bor    = function (a, b) return a | b end;
 	bxor   = function (a, b) return a ~ b end;
+	rshift = function (a, n) return a >> n end;
+	lshift = function (a, n) return a << n end;
 };
 
