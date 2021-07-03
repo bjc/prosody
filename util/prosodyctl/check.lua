0409851d7478f94213970f91afbbb870a0d7589d
Kim Alvefur <zash@zash.se>
2021-07-04 01:29:47 +0200

    util.prosodyctl.check: Warn if both use_ipv4 and use_ipv6 are set to false
    Why would you do this?!

--- util/prosodyctl/check.lua
+++ util/prosodyctl/check.lua
@@ -473,6 +473,12 @@ local function check(arg)
 
 			local use_ipv4 = configmanager.get("*", "use_ipv4") ~= false;
 			local use_ipv6 = configmanager.get("*", "use_ipv6") ~= false;
+			if not use_ipv4 and not use_ipv6 then
+				print("    Both IPv6 and IPv4 are disabled, Prosody will not listen on any ports");
+				print("    nor be able to connect to any remote servers.");
+				all_targets_ok = false;
+			end
+
 			for target_host in target_hosts do
 				local host_ok_v4, host_ok_v6;
 				do
