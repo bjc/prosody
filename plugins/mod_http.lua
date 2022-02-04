35e77293c0e2e0aecb8931bf4900cdccd20abcfe
Kim Alvefur <zash@zash.se>
2022-02-04 19:58:43 +0100

    mod_http: Use http_default_host for URLs generated in global context
    This might make the global routes less confusing sometimes, or at least
    valid URLs instead of http://*:5280/ which doesn't make much sense.

--- plugins/mod_http.lua
+++ plugins/mod_http.lua
@@ -94,7 +94,8 @@ function moduleapi.http_url(module, app_name, default_path)
 		for port, service in pairs(ports) do -- luacheck: ignore 512
 			local url = {
 				scheme = service[1].service.name;
-				host = module:get_option_string("http_host", module.host);
+				host = module:get_option_string("http_host", module.global
+					and module:get_option_string("http_default_host") or module.host);
 				port = port;
 				path = get_base_path(module, app_name, default_path or "/" .. app_name);
 			}
