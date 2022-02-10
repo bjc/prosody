d92ceb3adb759e3bc686709c5b7f4c92d10565c4
Kim Alvefur <zash@zash.se>
2022-02-10 15:39:15 +0100

    mod_turn_external: Simplify configuration
    Much harder to get boolean options wrong than accidentally adding
    something unrecognised to a Set.

--- plugins/mod_turn_external.lua
+++ plugins/mod_turn_external.lua
@@ -1,12 +1,19 @@
+local set = require "util.set";
+
 local secret = module:get_option_string("turn_external_secret");
 local host = module:get_option_string("turn_external_host", module.host);
 local user = module:get_option_string("turn_external_user");
 local port = module:get_option_number("turn_external_port", 3478);
 local ttl = module:get_option_number("turn_external_ttl", 86400);
-
-local services = module:get_option_set("turn_external_services", {"stun-udp"; "turn-udp"});
+local tcp = module:get_option_boolean("turn_external_tcp", false);
 
 if not secret then error("mod_" .. module.name .. " requires that 'turn_external_secret' be set") end
+
+local services = set.new({ "stun-udp"; "turn-udp" });
+if tcp then
+	services:add("stun-tcp");
+	services:add("turn-tcp");
+end
 
 module:depends "external_services";
 
