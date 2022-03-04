a905ccb71a571649c7aed90bf444e1afed9a1318
Matthew Wild <mwild1@gmail.com>
2022-03-04 19:37:59 +0000

    util.bit53: Support for more than 2 arguments, for compat with bit32

--- util/bit53.lua
+++ util/bit53.lua
@@ -1,8 +1,32 @@
 -- Only the operators needed by net.websocket.frames are provided at this point
 return {
-	band   = function (a, b) return a & b end;
-	bor    = function (a, b) return a | b end;
-	bxor   = function (a, b) return a ~ b end;
+	band   = function (a, b, ...)
+		local ret = a & b;
+		if ... then
+			for i = 1, select("#", ...) do
+				ret = ret & (select(i, ...));
+			end
+		end
+		return ret;
+	end;
+	bor    = function (a, b, ...)
+		local ret = a | b;
+		if ... then
+			for i = 1, select("#", ...) do
+				ret = ret | (select(i, ...));
+			end
+		end
+		return ret;
+	end;
+	bxor   = function (a, b, ...)
+		local ret = a ~ b;
+		if ... then
+			for i = 1, select("#", ...) do
+				ret = ret ~ (select(i, ...));
+			end
+		end
+		return ret;
+	end;
 	rshift = function (a, n) return a >> n end;
 	lshift = function (a, n) return a << n end;
 };
