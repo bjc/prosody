36afd999b1042b997ae59c81dce755fb01ee2b11
Matthew Wild <mwild1@gmail.com>
2022-10-06 15:59:07 +0100

    mod_tokenauth: Invalidate tokens issued before most recent password change
    This is a security improvement, to ensure that sessions authenticated using a
    token (note: not currently possible in stock Prosody) are invalidated just
    like password-authenticated sessions are.

--- plugins/mod_tokenauth.lua
+++ plugins/mod_tokenauth.lua
@@ -68,6 +68,12 @@ local function _get_parsed_token_info(token_id, token_user, token_host)
 		return nil, "not-authorized";
 	end
 
+	local account_info = usermanager.get_account_info(token_user, module.host);
+	local password_updated_at = account_info and account_info.password_updated;
+	if password_updated_at and password_updated_at > token_info.created then
+		return nil, "not-authorized";
+	end
+
 	return token_info
 end
 
