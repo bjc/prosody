60ae9199731ffa5b7c200fedda715097c453ff91
Kim Alvefur <zash@zash.se>
2023-03-24 16:29:48 +0100

    mod_server_contact_info: Reorder dataform keys for more aligned view

--- plugins/mod_server_contact_info.lua
+++ plugins/mod_server_contact_info.lua
@@ -7,19 +7,20 @@
 --
 
 local array = require "prosody.util.array";
+local dataforms = require "prosody.util.dataforms";
 local jid = require "prosody.util.jid";
 local url = require "socket.url";
 
 -- Source: http://xmpp.org/registrar/formtypes.html#http:--jabber.org-network-serverinfo
-local form_layout = require "prosody.util.dataforms".new({
-	{ var = "FORM_TYPE"; type = "hidden"; value = "http://jabber.org/network/serverinfo"; };
-	{ name = "abuse", var = "abuse-addresses", type = "list-multi" },
-	{ name = "admin", var = "admin-addresses", type = "list-multi" },
-	{ name = "feedback", var = "feedback-addresses", type = "list-multi" },
-	{ name = "sales", var = "sales-addresses", type = "list-multi" },
-	{ name = "security", var = "security-addresses", type = "list-multi" },
-	{ name = "status", var = "status-addresses", type = "list-multi" },
-	{ name = "support", var = "support-addresses", type = "list-multi" },
+local form_layout = dataforms.new({
+	{ var = "FORM_TYPE"; type = "hidden"; value = "http://jabber.org/network/serverinfo" };
+	{ type = "list-multi"; name = "abuse"; var = "abuse-addresses" };
+	{ type = "list-multi"; name = "admin"; var = "admin-addresses" };
+	{ type = "list-multi"; name = "feedback"; var = "feedback-addresses" };
+	{ type = "list-multi"; name = "sales"; var = "sales-addresses" };
+	{ type = "list-multi"; name = "security"; var = "security-addresses" };
+	{ type = "list-multi"; name = "status"; var = "status-addresses" };
+	{ type = "list-multi"; name = "support"; var = "support-addresses" };
 });
 
 -- JIDs of configured service admins are used as fallback
