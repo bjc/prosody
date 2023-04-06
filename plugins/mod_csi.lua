90ee187b23660d1690ecbfcad57553209a6cdede
Kim Alvefur <zash@zash.se>
2023-04-06 08:01:55 +0200

    mod_csi: Add metrics, covering changes and totals
    Motivation: Investigating clients that seem to forget to set CSI.
    Also, of course, MORE GRAPHS!

--- plugins/mod_csi.lua
+++ plugins/mod_csi.lua
@@ -2,6 +2,17 @@ local st = require "prosody.util.stanza";
 local xmlns_csi = "urn:xmpp:csi:0";
 local csi_feature = st.stanza("csi", { xmlns = xmlns_csi });
 
+local sum = module:metric("gauge", "sessions_per_state", "sessions", "CSI state per session", { "csi_state" })
+local change = module:metric("counter", "changes", "events", "CSI state changes", {"csi_state"});
+
+module:hook_global("stats-update", function ()
+	for _, session in pairs(prosody.full_sessions) do
+		if session.host == module.host then
+			sum:with_labels(session.state or "none"):add(1);
+		end
+	end
+end);
+
 local csi_handler_available = nil;
 module:hook("stream-features", function (event)
 	if event.origin.username and csi_handler_available then
@@ -13,6 +24,7 @@ function refire_event(name)
 	return function (event)
 		if event.origin.username then
 			event.origin.state = event.stanza.name;
+			change:with_labels(event.stanza.name):add(1);
 			module:fire_event(name, event);
 			return true;
 		end
