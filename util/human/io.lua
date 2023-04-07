b5ff6256b6ea968b77cd157fb55bdd0bd6e1ee3f
Matthew Wild <mwild1@gmail.com>
2023-04-07 12:35:39 +0100

    util.human.io: table: use term_width() to discover terminal width

--- util/human/io.lua
+++ util/human/io.lua
@@ -126,7 +126,7 @@ local function ellipsis(s, width)
 end
 
 local function new_table(col_specs, max_width)
-	max_width = max_width or tonumber(os.getenv("COLUMNS")) or 80;
+	max_width = max_width or term_width(os.getenv("COLUMNS") or 80);
 	local separator = " | ";
 
 	local widths = {};
