ffeea163934079730cf7da77b82b1804864380d0
Kim Alvefur <zash@zash.se>
2023-04-10 13:50:09 +0200

    mod_csi: Remove module status, doesn't work because of mod_smacks
    This was meant to warn in case you had only mod_csi without a logic
    handling module like mod_csi_simple by checking if anything hooked this
    event, however mod_smacks also hooks this event and so this isn't really
    a useful way of detecting this condition.

--- plugins/mod_csi.lua
+++ plugins/mod_csi.lua
@@ -24,15 +24,3 @@ end
 
 module:hook("stanza/"..xmlns_csi..":active", refire_event("csi-client-active"));
 module:hook("stanza/"..xmlns_csi..":inactive", refire_event("csi-client-inactive"));
-
-function module.load()
-	if prosody.hosts[module.host].events._handlers["csi-client-active"] then
-		csi_handler_available = true;
-		module:set_status("core", "CSI handler module loaded");
-	else
-		csi_handler_available = false;
-		module:set_status("warn", "No CSI handler module loaded");
-	end
-end
-module:hook("module-loaded", module.load);
-module:hook("module-unloaded", module.load);
