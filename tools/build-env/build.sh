d26ccd84004372051deeff7d68f2e9264f54055a
Kim Alvefur <zash@zash.se>
2023-12-12 16:19:04 +0100

    tools: Fix selection of container engine
    Seems command -v in sh only checks and returns one argument, unlike
    bash.

--- tools/build-env/build.sh
+++ tools/build-env/build.sh
@@ -2,7 +2,12 @@
 
 cd "$(dirname "$0")"
 
-containerify="$(command -v podman docker)"
+containerify="$(command -v podman || command -v docker)"
+
+if [ -z "$containerify" ]; then
+	echo "podman or docker required" >&2
+	exit 1
+fi
 
 $containerify build -f ./Containerfile --squash \
 	--build-arg os="${2:-debian}" \
