918e8f51e82cd26898d0a3aa8d178ad50fc7e922
Matthew Wild <mwild1@gmail.com>
2025-01-14 12:26:14 +0000

    mod_pubsub: Remove duplicate create_node command
    Removed the one without error handling.

--- plugins/mod_pubsub/commands.lib.lua
+++ plugins/mod_pubsub/commands.lib.lua
@@ -7,22 +7,6 @@ local function add_commands(get_service)
 	module:add_item("shell-command", {
 		section = "pubsub";
 		section_desc = "Manage publish/subscribe nodes";
-		name = "create_node";
-		desc = "Create a node with the specified name";
-		args = {
-			{ name = "service_jid", type = "string" };
-			{ name = "node_name",   type = "string" };
-		};
-		host_selector = "service_jid";
-
-		handler = function (self, service_jid, node_name) --luacheck: ignore 212/self
-			return get_service(service_jid):create(node_name, true);
-		end;
-	});
-
-	module:add_item("shell-command", {
-		section = "pubsub";
-		section_desc = "Manage publish/subscribe nodes";
 		name = "list_nodes";
 		desc = "List nodes on a pubsub service";
 		args = {
